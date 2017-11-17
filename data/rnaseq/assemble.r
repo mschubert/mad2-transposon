b240c552e3cf6bf9a951e1fff6ec58a1917c5df6
mschubert <mschu.dev@gmail.com>
2017-11-17 15:45:23 +0100

    add plots

--- data/rnaseq/assemble.r
+++ data/rnaseq/assemble.r
@@ -1,5 +1,5 @@
 library(dplyr)
-library(factoextra)
+library(ggrepel)
 plt = import('plot')
 rnaseq = import('process/rna-seq')
 
@@ -17,16 +17,25 @@ colnames(expr) = samples$id
 
 # do PCA/dim reduction plots to see how they cluster
 pca = prcomp(t(expr), scale=FALSE)
-ggplot(cbind(samples, pca$x), aes(x=PC1, y=PC2, color=tissue, shape=`Early/ Late`)) +
+p1 = ggplot(cbind(samples, pca$x), aes(x=PC1, y=PC2, color=`Tumour type`, shape=`Early/ Late`)) +
     geom_point(size=5) +
-    geom_text(aes(label=id), color="black") +
+    geom_text_repel(aes(label=id), color="black") +
     labs(x = sprintf("PC1 (%.1f%%)", summary(pca)$importance[2,1]*100),
-         y = sprintf("PC2 (%.1f%%)", summary(pca)$importance[2,2]*100))
+         y = sprintf("PC2 (%.1f%%)", summary(pca)$importance[2,2]*100),
+         title = "PCA plot (linear)")
 
 tsne = Rtsne::Rtsne(t(expr), perplexity=10)
-cbind(samples, x=tsne$Y[,1], y=tsne$Y[,2]) %>%
-    ggplot(aes(x=x, y=y, color=tissue, shape=`Early/ Late`)) +
+p2 = cbind(samples, x=tsne$Y[,1], y=tsne$Y[,2]) %>%
+    ggplot(aes(x=x, y=y, color=`Tumour type`, shape=`Early/ Late`)) +
     geom_point(size=5) +
-    geom_text(aes(label=id), color="black")
+    geom_text_repel(aes(label=id), color="black") +
+    labs(x = "tsne 1",
+         y = "tsne 2",
+         title = "T-SNE plot (non-linear)")
+
+pdf("overview.pdf", width=10, height=8)
+print(p1)
+print(p2)
+dev.off()
 
 # save in merged object
