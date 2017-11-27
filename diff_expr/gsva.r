18508b524948e47eccb216c954e416a88ae2ede5
mschubert <mschu.dev@gmail.com>
2017-11-27 11:13:43 +0100

    restructure

--- diff_expr/gsva.r
+++ diff_expr/gsva.r
@@ -2,6 +2,7 @@ library(DESeq2)
 b = import('base')
 io = import('io')
 st = import('stats')
+plt = import('plot')
 gs = import('data/genesets')
 idmap = import('process/idmap')
 
@@ -32,7 +33,7 @@ if (is.null(module_name())) {
     cond = cbind(intercept = rep(1, nrow(index)),
                  thymus_over_spleen = index$tissue == "thymus",
                  other_over_spleen = index$tissue == "other",
-                 myeloid_leukemia = index$type == "Myeloid leukaemia",
+                 myeloid_leukemia = index$type == "Myeloid leukemia",
                  stage_late = index$stage == "Late",
                  mad2_high = index$mad2 == "High") + 0
     rownames(cond) = index$id
@@ -52,6 +53,28 @@ if (is.null(module_name())) {
         mutate(gene = idmap$gene(gene, to="external_gene_name", dset="mmusculus_gene_ensembl"))
     assocs_sets = st$lm(scores ~ 0 + cond, atomic="cond") %>% process()
 
-    write.table(assocs_gene, file="de_genes.tsv", row.names=FALSE, quote="", sep="\t")
-    write.table(assocs_sets, file="de_sets.tsv", row.names=FALSE, quote="", sep="\t")
+    #TODO?: abs(scale(expr)) if parts of set up+down
+
+    write.table(assocs_gene, file="de_genes.tsv", row.names=FALSE, quote=FALSE, sep="\t")
+    write.table(assocs_sets, file="de_sets.tsv", row.names=FALSE, quote=FALSE, sep="\t")
+
+    #TODO: volcano plot
+    do_plot = function(df, cond, repel=TRUE, label_top=50, ...) df %>%
+        filter(term == cond) %>%
+        mutate(label = scores, size=5) %>%
+        plt$p_effect("adj.p", thresh=0.1) %>%
+        plt$volcano(..., repel=repel, label_top=label_top)
+
+    assocs_gene$scores = assocs_gene$gene
+    pdf("volcano_genes.pdf", width=10, height=10)
+    for (subs in unique(assocs_gene$term))
+        print(do_plot(assocs_gene, subs) + ggtitle(subs))
+    dev.off()
+
+    pdf("volcano_sets.pdf", width=10, height=10)
+    for (subs in unique(assocs_sets$term))
+        print(do_plot(assocs_sets, subs, label_top=20) + ggtitle(subs))
+    dev.off()
+
+    #TODO: network enrichment plot(?)
 }
