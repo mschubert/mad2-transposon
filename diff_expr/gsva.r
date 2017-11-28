113b498259db4539c218b3fe7d90433cc13a3727
mschubert <mschu.dev@gmail.com>
2017-11-28 18:04:14 +0100

    regress types, fit aneuploidy

--- diff_expr/gsva.r
+++ diff_expr/gsva.r
@@ -8,15 +8,17 @@ idmap = import('process/idmap')
 
 if (is.null(module_name())) {
     # load the gene expression data + sample types
-    dset = io$load('assemble.RData')
+    aneuploidy = io$load('../aneuploidy/ploidy_from_rnaseq.RData')$aneuploidy
+    dset = io$load('../data/rnaseq/assemble.RData')
     expr = dset$expr[rowSums(dset$counts) >= 10,]
     index = dset$idx %>%
         transmute(id = id,
                   tissue = tissue,
                   type = `Tumour type`,
                   mad2 = `Mad2 levels`,
-                  stage = `Early/ Late`)
-    index$tissue[is.na(index$tissue)] = "other"
+                  stage = `Early/ Late`) %>%
+        full_join(data.frame(id=names(aneuploidy), aneuploidy=unname(aneuploidy)), by = "id")
+    index$tissue[is.na(index$tissue)] = "other" # should this be spleen?
 
     # load Gene Ontology sets from Ensembl
     go = gs$go(dset="mmusculus_gene_ensembl", genes="ensembl_gene_id") %>%
@@ -30,28 +32,12 @@ if (is.null(module_name())) {
     # no need to scale expr here, GSVA does it
     scores = GSVA::gsva(expr, go, kernel=FALSE, parallel.sz=1) %>% t()
 
-    cond = cbind(intercept = rep(1, nrow(index)),
-                 thymus_over_spleen = index$tissue == "thymus",
-                 other_over_spleen = index$tissue == "other",
-                 myeloid_leukemia = index$type == "Myeloid leukemia",
-                 stage_late = index$stage == "Late",
-                 mad2_high = index$mad2 == "High") + 0
-    rownames(cond) = index$id
-
-    process = . %>%
-        mutate(term = sub("^cond", "", term)) %>%
-        filter(term != "intercept") %>%
-        select(-size) %>%
-        group_by(term) %>%
-        mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
-        ungroup() %>%
-        arrange(adj.p, p.value) %>%
-        filter(adj.p < 0.2)
-
     gene = t(expr)
-    assocs_gene = st$lm(gene ~ 0 + cond, atomic="cond") %>% process() %>%
-        mutate(gene = idmap$gene(gene, to="external_gene_name", dset="mmusculus_gene_ensembl"))
-    assocs_sets = st$lm(scores ~ 0 + cond, atomic="cond") %>% process()
+    assocs_gene = st$lm(gene ~ type + aneuploidy, data=index) %>%
+        mutate(gene = idmap$gene(gene, to="external_gene_name", dset="mmusculus_gene_ensembl")) %>%
+        arrange(p.value)
+    assocs_sets = st$lm(scores ~ type + aneuploidy, data=index) %>%
+        arrange(p.value)
 
     #TODO?: abs(scale(expr)) if parts of set up+down
 
@@ -61,9 +47,9 @@ if (is.null(module_name())) {
     #TODO: volcano plot
     do_plot = function(df, cond, repel=TRUE, label_top=50, ...) df %>%
         filter(term == cond) %>%
-        mutate(label = scores, size=5) %>%
+        mutate(adj.p=p.adjust(p.value, method="fdr"), label = scores, size=5) %>%
         plt$p_effect("adj.p", thresh=0.1) %>%
-        plt$volcano(..., repel=repel, label_top=label_top)
+        plt$volcano(..., check_overlap=TRUE, label_top=label_top)
 
     assocs_gene$scores = assocs_gene$gene
     pdf("volcano_genes.pdf", width=10, height=10)
