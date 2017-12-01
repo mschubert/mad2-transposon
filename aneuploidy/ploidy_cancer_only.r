40305dd2e15bde5d3d90b0e40c3d865248185a3b
mschubert <mschu.dev@gmail.com>
2017-12-01 15:30:21 +0100

    add ploidy from mad2 ref

---
+++ aneuploidy/ploidy_cancer_only.r
@@ -0,0 +1,71 @@
+library(cowplot)
+library(ggrepel)
+io = import('io')
+df = import('data_frame')
+idmap = import('process/idmap')
+aneufinder = import('../../aneuploidy/data/singlecell_wgs/aneufinder')
+
+# reads per chromosome for T-ALLs
+dset = io$load('../data/rnaseq/assemble.RData')
+chrs = idmap$gene(rownames(dset$counts), to="chromosome_name", dset="mmusculus_gene_ensembl")
+reads = dset$counts[!is.na(chrs),]
+chrs = chrs[!is.na(chrs)]
+reads = narray::map(reads, along=1, sum, subsets=chrs)[as.character(1:19),]
+reads = reads / narray::rrep(colSums(reads), nrow(reads))
+names(dimnames(reads)) = c("seqnames", "sample")
+read_df = reshape2::melt(reads, value.name = "frac_reads") %>%
+    mutate(seqnames = as.character(seqnames),
+           sample = as.character(sample))
+
+# ploidy from scWGS
+wgs = c("T401", "T419", "S413") %>%
+    paste0("../../aneuploidy/data/singlecell_wgs/T-ALL/", ., ".RData") %>%
+    io$load() %>%
+    lapply(aneufinder$consensus_ploidy) %>%
+    setNames(c("401t", "419t", "413s")) %>%
+    df$bind_rows("sample")
+
+# calculate k^chr (cf. chr_ngenes_nreads_cor.r)
+kchr = wgs %>%
+    dplyr::left_join(read_df, by=c("sample", "seqnames")) %>%
+    mutate(kchr = ploidy / frac_reads)
+
+# aneuploidy scores for all samples
+kk = kchr %>%
+    group_by(seqnames) %>%
+    summarize(kchr = mean(kchr))
+
+aneuploidy_separate = read_df %>%
+    left_join(kk, by="seqnames") %>%
+    mutate(ploidy = frac_reads * kchr)
+
+aneuploidy_mean = aneuploidy_separate %>%
+    group_by(sample, seqnames) %>%
+    summarize(ploidy = mean(ploidy))
+
+aneuploidy_min = aneuploidy_separate %>%
+    group_by(sample, seqnames) %>%
+    summarize(ploidy = sign(ploidy) * (2 + min(abs(ploidy)-2)))
+
+# sanity check for samples where we have both
+kchr_ctl = kchr %>% transmute(seqnames=seqnames, real_ploidy=ploidy, ref=sample)
+ctl = aneuploidy_separate %>%
+    inner_join(kchr_ctl, by=c("seqnames")) %>%
+    filter(sample %in% c("401t", "419t", "413s"),
+           sample != ref)
+
+mod = lm(ploidy ~ real_ploidy, data=ctl)
+p = ggplot(ctl, aes(x=real_ploidy, y=ploidy, label=seqnames, color=sample)) +
+    geom_text() +
+    geom_smooth(method="lm", color="black") +
+    labs(x = "Ploidy from single-cell WGS data + AneuFinder",
+         y = "Ploidy inferred from RNA-seq",
+         title = sprintf("RNA-seq for ploidy inference (p=%.2g, r^2=%.2f)",
+             mod %>% broom::tidy() %>% filter(term == "real_ploidy") %>% pull(p.value),
+             mod %>% broom::glance() %>% pull(r.squared)))
+
+pdf("ploidy_cancer_only.pdf")
+print(p)
+dev.off()
+
+save(ploidy, aneuploidy, wgs, file="ploidy_from_rnaseq.RData")
