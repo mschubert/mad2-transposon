533da1d1d1033d4e56b35330ae54aacd757123ea
mschubert <mschu.dev@gmail.com>
2017-12-13 14:35:30 +0100

    add aneuploidy test

--- cis_integration/cis_fet.r
+++ cis_integration/cis_fet.r
@@ -21,6 +21,8 @@ test_gene = function(df) {
     fisher.test(mat) %>%
         broom::tidy() %>%
         transmute(condition = df$condition[1],
+                  ins_cond = df$ins_gene[1],
+                  ins_rest = df$ins_gene[2],
                   log_odds = estimate,
                   p.value = p.value)
 }
@@ -35,12 +37,12 @@ mad2 = filter(both, aneup_class != "high") %>%
 keep = mad2 %>%
     group_by(gene) %>%
     summarize(ins_gene = sum(ins_gene)) %>%
-    filter(ins_gene >= 3) %>%
+    filter(ins_gene >= 5) %>%
     pull(gene) %>%
     as.character()
 mad2 = mad2[mad2$gene %in% keep,]
 
-re = mad2 %>%
+mad2 = mad2 %>%
     group_by(gene) %>%
     tidyr::nest() %>%
     mutate(result = purrr::map(data, test_gene)) %>%
@@ -48,4 +50,33 @@ re = mad2 %>%
     tidyr::unnest() %>%
     mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
     arrange(p.value) %>%
-    filter(adj.p < 0.2)
+    filter(adj.p < 0.5)
+
+
+
+aneup = filter(both, mad2_class == "low") %>%
+    group_by(aneup_class, gene) %>%
+    summarize(ins_gene = sum(n_ins)) %>%
+    ungroup() %>%
+    group_by(aneup_class) %>%
+    mutate(ins_total = sum(ins_gene),
+           condition = paste0("aneup-", aneup_class))
+keep = aneup %>%
+    group_by(gene) %>%
+    summarize(ins_gene = sum(ins_gene)) %>%
+    filter(ins_gene >= 5) %>%
+    pull(gene) %>%
+    as.character()
+aneup = aneup[aneup$gene %in% keep,]
+
+aneup = aneup %>%
+    group_by(gene) %>%
+    tidyr::nest() %>%
+    mutate(result = purrr::map(data, test_gene)) %>%
+    select(-data) %>%
+    tidyr::unnest() %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+    arrange(p.value) %>%
+    filter(adj.p < 0.5)
+
+save(mad2, aneup, file="cis_fet.RData")
