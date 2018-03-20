87c7e73c824e0677fa4a3c07911467cdd2421e82
mschubert <mschu.dev@gmail.com>
2018-03-20 16:02:03 +0000

    add aneuploidy scores

--- ploidy_from_wgs/non_integer.r
+++ ploidy_from_wgs/non_integer.r
@@ -2,6 +2,7 @@ library(ggbio)
 library(dplyr)
 library(magrittr)
 io = import('io')
+seq = import('seq')
 sys = import('sys')
 
 plot_cov = function(bins, segs, counts="counts", bin_y="copy.number", seg_y="mean.counts",
@@ -47,7 +48,12 @@ sys$run({
         rpp_mode = den$x[den$y==max(den$y)] / 2
         p2 = plot_cov(m$bins, m$segments, reads_per_ploidy=rpp_mode)
 
-        p = tracks(title = sprintf("%s", m$ID), #TODO: add aneuploidy score here
+        aneup = as.data.frame(m$segments) %>%
+            mutate(Sample = m$ID,
+                   ploidy = mean.counts / rpp_mode) %>%
+            seq$aneuploidy() %$% aneuploidy
+
+        p = tracks(title = sprintf("%s - aneuploidy: %.2f", m$ID, aneup),
                    Aneufinder = p1,
                    `Mode diploid` = p2)
         print(p)
