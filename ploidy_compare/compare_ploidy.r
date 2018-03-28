5ce93eb5e059968c3bc30075a397dfc7d1f2de0b
mschubert <mschu.dev@gmail.com>
2018-03-28 14:53:39 +0000

    working plot

--- ploidy_compare/compare_ploidy.r
+++ ploidy_compare/compare_ploidy.r
@@ -7,29 +7,30 @@ io = import('io')
 #    opt('i', 'infile', 'RData aneuploidy scores', 'copy_segments.RData'),
 #    opt('p', 'plotfile', 'pdf to save plot to', '/dev/null'))
 
-dna = io$load("../ploidy_from_wgs/copy_segments.RData")
-rna = io$load("../ploidy_from_rnaseq/panel_ploidy.RData")
+dna = io$load("../ploidy_from_wgs/copy_segments.RData")$aneups %>%
+    dplyr::rename(sample = Sample) %>%
+    mutate(sample = sub("(-high)|(-low)", "", sample))
+rna = io$load("../ploidy_from_rnaseq/panel_ploidy.RData")$aneup %>%
+    mutate(sample = toupper(gsub("[^0-9stST]+", "", sample)))
 
-aneups = list(
-        WGS = dna$aneups %>% rename(sample = Sample),
-        `RNA-seq` = rna$aneups %>%
-            mutate(sample = toupper(gsub("[^0-9ST]", "", sample)))) %>%
-    dplyr::bind_rows("type") %>%
+aneups = list(WGS = dna, `RNA-seq` = rna) %>%
+    dplyr::bind_rows(.id="type") %>%
     mutate(sample = forcats::fct_reorder(sample, aneuploidy))
 
-dens = ggplot(aneup, aes(x=aneuploidy)) +
-    geom_density(aes(fill="red"), alpha=0.5) +
+dens = ggplot(aneups, aes(x=aneuploidy)) +
+    geom_density(aes(fill=type), alpha=0.5) +
     theme(axis.title.x=element_blank(),
           axis.text=element_blank(),
           axis.line=element_blank(),
-          axis.ticks=element_blank())
+          axis.ticks=element_blank()) +
+    guides(fill=FALSE)
 
-samples = ggplot(aneup, aes(x=aneuploidy, y=sample, color=type)) +
-#    geom_vline(xintercept=0.05, color="blue", linetype="dashed") +
-    geom_segment(aes(xend=aneuploidy, y=sample, yend=sample), x=0, color="lightgrey") +
-    geom_point() +
+samples = ggplot(aneups, aes(x=aneuploidy, y=sample)) +
+    geom_segment(aes(xend=aneuploidy, yend=sample), x=0,
+                 color="lightgrey") +
+    geom_point(aes(color=type)) +
     theme(axis.text.y = element_text(size=8))
 
-pdf(4, 10, file=args$plotfile)
+pdf(7, 9, file="compare_ploidy.pdf")
 print(dens + samples + plot_layout(ncol=1, heights=c(1,12)))
 dev.off()
