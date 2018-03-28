99fe26c13eb523f7913007ba367d5f8a05e5681b
mschubert <mschu.dev@gmail.com>
2018-03-28 15:35:53 +0000

    add eT, cor matrix

--- ploidy_compare/compare_ploidy.r
+++ ploidy_compare/compare_ploidy.r
@@ -12,10 +12,18 @@ dna = io$load("../ploidy_from_wgs/copy_segments.RData")$aneups %>%
     mutate(sample = sub("(-high)|(-low)", "", sample))
 rna = io$load("../ploidy_from_rnaseq/panel_ploidy.RData")$aneup %>%
     mutate(sample = toupper(gsub("[^0-9stST]+", "", sample)))
+eT = io$load("../tis_rna-ploidy_cis-fet/aneuploidy_mad2.RData") %>%
+    transmute(sample=sample, aneuploidy=aneup)
 
-aneups = list(WGS = dna, `RNA-seq` = rna) %>%
+tissues = setNames(c("spleen", "thymus"), c("S","T"))
+
+aneups = list(WGS = dna, `RNA-seq (eDivisive)` = rna, `RNA-seq (eT)` = eT) %>%
     dplyr::bind_rows(.id="type") %>%
-    mutate(sample = forcats::fct_reorder(sample, aneuploidy))
+    group_by(type) %>%
+    mutate(aneuploidy = scale(aneuploidy, center=FALSE)) %>%
+    ungroup() %>%
+    mutate(sample = forcats::fct_reorder(sample, aneuploidy),
+           tissue = tissues[sub("Healthy|[0-9]+", "", sample)])
 
 dens = ggplot(aneups, aes(x=aneuploidy)) +
     geom_density(aes(fill=type), alpha=0.5) +
@@ -28,9 +36,16 @@ dens = ggplot(aneups, aes(x=aneuploidy)) +
 samples = ggplot(aneups, aes(x=aneuploidy, y=sample)) +
     geom_segment(aes(xend=aneuploidy, yend=sample), x=0,
                  color="lightgrey") +
-    geom_point(aes(color=type)) +
+    geom_point(aes(color=type, shape=tissue)) +
     theme(axis.text.y = element_text(size=8))
 
-pdf(7, 9, file="compare_ploidy.pdf")
+paircor = aneups %>%
+    group_by(sample, type, tissue) %>%
+    summarize(aneuploidy = mean(aneuploidy)) %>%
+    tidyr::spread("type", "aneuploidy") %>%
+    GGally::ggpairs(columns=3:5, aes(shape=tissue))
+
+pdf(9, 9, file="compare_ploidy.pdf")
 print(dens + samples + plot_layout(ncol=1, heights=c(1,12)))
+print(paircor)
 dev.off()
