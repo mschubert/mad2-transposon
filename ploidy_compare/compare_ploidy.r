0a6edbe5ee990febf8d178da6058532f289ea737
mschubert <mschu.dev@gmail.com>
2018-04-18 12:06:59 +0000

    export merged ploidy

--- ploidy_compare/compare_ploidy.r
+++ ploidy_compare/compare_ploidy.r
@@ -10,9 +10,7 @@ seq = import('seq')
 
 dna = io$load("../ploidy_from_wgs/copy_segments.RData")$segments %>%
     seq$aneuploidy() %>%
-    dplyr::rename(sample = Sample) %>%
-    mutate(sample = sub("(-high)|(-low)", "", sample),
-           sample = paste0(sub("[^ST]+", "", sample), sub("[^0-9]+", "", sample)))
+    dplyr::rename(sample = Sample)
 rna = io$load("../ploidy_from_rnaseq/panel_ploidy.RData")$segments %>%
     seq$aneuploidy(sample="sample", ploidy="expr") %>%
     mutate(sample = toupper(gsub("[^0-9stST]+", "", sample)))
@@ -22,13 +20,28 @@ eT2 = io$load("../ploidy_from_rnaseq/eT_ploidy.RData")$segments %>%
 eT = io$load("../tis_rna-ploidy_cis-fet/aneuploidy_mad2.RData") %>%
     transmute(sample=sample, aneuploidy=aneup)
 
+dna_merge = readr::read_tsv("wgs_merge.tsv") %>%
+    left_join(dna %>% select(subset=sample, aneuploidy))
+dna_label = dna %>%
+    mutate(sample = sub("(-high)|(-low)", "", sample),
+           sample = paste0(sub("[^ST]+", "", sample), sub("[^0-9]+", "", sample)))
+
 tissues = setNames(c("spleen", "thymus"), c("S","T"))
 
-#aneups = list(WGS = dna, `RNA-seq (eDivisive)` = rna, `RNA-seq (eT)` = eT, `RNA-seq (eT, new)` = eT2) %>%
-aneups = list(WGS = dna, `RNA-seq (eT)` = eT2) %>%
+aneups = list(WGS = dna_label,
+              `WGS (merged)` = dna_merge,
+              `RNA-seq (eT)` = eT2) %>%
     dplyr::bind_rows(.id="type") %>%
     mutate(sample = forcats::fct_reorder(sample, aneuploidy),
            tissue = tissues[sub("Healthy|[0-9]+", "", sample)])
+
+aneups %>%
+    group_by(type, sample, tissue) %>%
+    summarize(aneuploidy = ifelse(length(aneuploidy) == 1,
+        aneuploidy, weighted.mean(aneuploidy, weight))) %>%
+    ungroup() %>%
+    mutate(type = ifelse(grepl("WGS", type), "30-cell WGS", type)) %>%
+    write.table("compare_ploidy.tsv", sep="\t", quote=FALSE)
 
 dens = ggplot(aneups, aes(x=aneuploidy)) +
     geom_density(aes(fill=type), alpha=0.5) +
