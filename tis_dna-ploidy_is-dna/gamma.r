e2026278c2c59f5b705e508b99bf524bb884cca3
mschubert <mschu.dev@gmail.com>
2018-04-27 18:35:43 +0200

    add gamma, ks

---
+++ tis_dna-ploidy_is-dna/gamma.r
@@ -0,0 +1,59 @@
+# logistic regression
+#
+# 20(ish) read cutoff per transposon insertion
+# aneuploidy scores as continuous variable
+# test whether or not each gene has insertion w/ increasing aneuploidy score
+library(dplyr)
+io = import('io')
+sys = import('sys')
+
+args = sys$cmd$parse(
+    opt('r', 'reads', 'min number of reads', '20'),
+    opt('s', 'samples', 'min samples w/ insertions in gene', '5'),
+    opt('o', 'outfile', 'file to save to', 'logit.RData'))
+
+cis = io$load("dset.RData")
+samples = cis %>%
+    select(sample, aneup, n_ins_smp) %>%
+    distinct() %>%
+    mutate(reads = 0)
+
+do_fit = function(data) {
+    no_reads = samples %>% filter(!sample %in% data$sample)
+    data2 = dplyr::bind_rows(data, no_reads) %>%
+        mutate(reads = reads >= as.integer(args$reads))
+    stopifnot(nrow(data2) == nrow(samples))
+
+#mod = glm(reads ~ aneup, family=binomial(link='logit'), data=data2)
+#    mod = glm(reads ~ aneup, family=quasipoisson(), data=data2)
+
+    mod = glm(aneup ~ reads, family=Gamma(), data=data2)
+    mod$model = na.omit(select(data2, sample, aneup, reads))
+    mod %>%
+        broom::tidy() %>%
+        filter(term == "readsTRUE") %>%
+        select(-term) %>%
+        mutate(size = length(unique(data$sample)),
+               mod = list(mod))
+}
+
+assoc = . %>%
+    group_by(sample, gene_name, ensembl_gene_id, aneup, n_ins_smp) %>%
+    summarize(reads = sum(reads)) %>%
+    group_by(gene_name, ensembl_gene_id) %>%
+    filter(sum(!is.na(aneup) & reads >= as.integer(args$reads)) >=
+           as.integer(args$sample)) %>%
+    tidyr::nest() %>%
+    mutate(fit = purrr::map(data, do_fit)) %>%
+    select(-data) %>%
+    tidyr::unnest() %>%
+    arrange(p.value) %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+    select(-mod, mod)
+
+hits = assoc(filter(cis, hit_dist == 0))
+hits_cancer = assoc(filter(cis, hit_dist == 0, known_cancer))
+near = assoc(cis)
+near_cancer = assoc(filter(cis, known_cancer))
+
+save(hits, hits_cancer, near, near_cancer, file=args$outfile)
---
+++ tis_dna-ploidy_is-dna/ks.r
@@ -0,0 +1,56 @@
+# logistic regression
+#
+# 20(ish) read cutoff per transposon insertion
+# aneuploidy scores as continuous variable
+# test whether or not each gene has insertion w/ increasing aneuploidy score
+library(dplyr)
+io = import('io')
+sys = import('sys')
+
+args = sys$cmd$parse(
+    opt('r', 'reads', 'min number of reads', '20'),
+    opt('s', 'samples', 'min samples w/ insertions in gene', '5'),
+    opt('o', 'outfile', 'file to save to', 'ks.RData'))
+
+cis = io$load("dset.RData")
+samples = cis %>%
+    select(sample, aneup, n_ins_smp) %>%
+    distinct() %>%
+    mutate(reads = 0)
+
+do_fit = function(data) {
+    no_reads = samples %>% filter(!sample %in% data$sample)
+    data2 = dplyr::bind_rows(data, no_reads) %>%
+        mutate(reads = reads >= as.integer(args$reads))
+    stopifnot(nrow(data2) == nrow(samples))
+
+    x = data2 %>% arrange(aneup) %>% na.omit()
+    mod = ks.test(x$aneup, x$aneup[x$reads])
+    mod$model = na.omit(select(data2, sample, aneup, reads))
+    mod %>%
+        broom::tidy() %>%
+        mutate(estimate = mean(x$aneup[x$reads]) - mean(x$aneup),
+               size = length(unique(data$sample)),
+               mod = list(mod))
+}
+
+assoc = . %>%
+    group_by(sample, gene_name, ensembl_gene_id, aneup, n_ins_smp) %>%
+    summarize(reads = sum(reads)) %>%
+    group_by(gene_name, ensembl_gene_id) %>%
+    filter(sum(!is.na(aneup) & reads >= as.integer(args$reads)) >=
+           as.integer(args$sample)) %>%
+    tidyr::nest() %>%
+    mutate(fit = purrr::map(data, do_fit)) %>%
+    select(-data) %>%
+    tidyr::unnest() %>%
+    arrange(p.value) %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+    select(-mod, mod)
+
+hits = assoc(filter(cis, hit_dist == 0))
+hits_cancer = assoc(filter(cis, hit_dist == 0, known_cancer))
+near = assoc(cis)
+near_cancer = assoc(filter(cis, known_cancer))
+
+save(hits, hits_cancer, near, near_cancer, file=args$outfile)
