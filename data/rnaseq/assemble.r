008a816be704b7955a6d023bb90ecc4b1f00445d
mschubert <mschu.dev@gmail.com>
2018-07-23 20:36:26 +0000

    assemble annots; imfusion workflow workarounds

--- data/rnaseq/assemble.r
+++ data/rnaseq/assemble.r
@@ -9,7 +9,9 @@ load_file = function(fname) {
     batch = b$grep("batch([0-9]+)", fname)
     ee = cont$counts
     colnames(ee) = paste(batch, colnames(ee), sep="_")
-    list(idx = idx %>% mutate(sample = paste(batch, sample, sep="_")),
+    list(idx = cont$idx %>%
+            mutate(sample = paste(batch, sample, sep="_"),
+                   batch = batch),
          counts = ee)
 }
 cont = lapply(sprintf("Mad2+PB_batch%i.RData", 1:3), load_file)
@@ -21,8 +23,14 @@ expr = lapply(cont, function(x) x$counts) %>%
     util$rnaseq$vst()
 dd = as.matrix(dist(t(expr)))
 
-pdf(OUTFILE, 14, 14)
-pheatmap::pheatmap(dd, col=colorRampPalette(rev(brewer.pal(9, "Blues")))(255))
+annot = as.data.frame(idx)
+rownames(annot) = idx$sample
+annot$hist_nr = NULL
+annot$sample = NULL
+
+pdf(OUTFILE, 16, 14)
+pheatmap::pheatmap(dd, col=colorRampPalette(rev(brewer.pal(9, "Blues")))(255),
+                   annotation=annot)
 print(util$plot_pca(expr, idx))
 print(util$plot_tsne(expr, idx))
 dev.off()
