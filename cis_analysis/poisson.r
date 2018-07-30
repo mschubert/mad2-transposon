5164231704804fe632621530affad5c23d0ca87c
mschubert <mschu.dev@gmail.com>
2018-07-30 11:45:48 +0000

    calc from TTAA sites

--- cis_analysis/poisson.r
+++ cis_analysis/poisson.r
@@ -12,39 +12,30 @@ args = sys$cmd$parse(
     opt('d', 'downstream', 'bp to include after gene', '0'),
     opt('o', 'outfile', 'insertion statistics RData', 'poisson.RData'))
 
-# collapse close insertion sites per sample?
 cis = io$load(args$infile) %>%
+    filter(chr %in% c(1:19, 'X')) %>%
     GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE,
         start.field="position", end.field="position")
 
-#TODO:
-# function: for each chromosome, count TTAA and AATT; also count per gene
-# use on all chrs
-genome = seq$genome("GRCm38")
-# subset default chromosomes
-# construct GRanges with all TTAA sites
-# count sites in genes and use as gene length
-ttaa = Biostrings::matchPattern("TTAA", genome$`1`)
-aatt = Biostrings::matchPattern("AATT", genome$`1`)
-# count each region only once per sample?
+genome = seq$genome("GRCm38", chrs=c(1:19, 'X'))
+ins_sites_total = seq$count_pattern("TTAA", genome, rc=TRUE)
+ins_per_sample = length(cis) / length(unique(cis$sample))
+ins_rate = ins_per_sample / ins_sites_total
 
 #stretch(anchor_3p(genes), as.integer(args$upstream)) #TODO: check if this works
 #stretch(anchor_5p(genes), as.integer(args$downstream))
 genes = seq$coords$gene(dset="mmusculus_gene_ensembl", granges=TRUE) %>%
     filter(seqnames(.) %in% seqnames(cis)) %>%
     select(external_gene_name) %>% # ...stretch here...
-    mutate(cis = count_overlaps(., cis)) %>%
+    mutate(TTAAs = seq$count_pattern("TTAA", genome, ., rc=TRUE),
+           cis = count_overlaps(., cis)) %>%
     arrange(-cis)
-
-glen = seq$lengths(seq$genome("GRCm38"))
-glen = glen[names(glen) %in% seqnames(cis)]
-n_smp = length(unique(cis$sample))
-rate = sum(genes$cis) / (sum(glen) * n_smp)
 
 do_test = function(n_ins, gene_len, rate)
     poisson.test(n_ins, gene_len * n_smp, rate)$p.value
+
 result = tbl_df(as.data.frame(genes)) %>%
-    mutate(p.value = purrr::map2_dbl(cis, width, do_test, rate=rate*2),
+    mutate(p.value = purrr::map2_dbl(cis, TTAAs, do_test, rate=ins_rate),
            adj.p = p.adjust(p.value, method="fdr")) %>%
     arrange(adj.p, p.value)
 
