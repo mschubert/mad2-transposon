9f549a91d7abd3a4b6d8b5d5df30f9076a983aeb
mschubert <mschu.dev@gmail.com>
2018-07-30 08:37:54 +0000

    use analysis set

--- cis_analysis/poisson.r
+++ cis_analysis/poisson.r
@@ -7,18 +7,26 @@ sys = import('sys')
 #TODO: only allow one insertion per sample and region?
 
 args = sys$cmd$parse(
-    opt('i', 'infile', 'cis RData', '../data/cis/cis_per_tumor.RData'),
+    opt('i', 'infile', 'cis RData', 'analysis_set.RData'),
     opt('u', 'upstream', 'bp to include before gene', '1000'), #TOOD: 10k (?)
     opt('d', 'downstream', 'bp to include after gene', '0'),
-    opt('r', 'reads', 'min num of reads', '20'),
-    opt('o', 'outfile', 'insertion statistics RData', 'interval_match_fet.RData'))
+    opt('o', 'outfile', 'insertion statistics RData', 'poisson.RData'))
 
+# collapse close insertion sites per sample?
 cis = io$load(args$infile) %>%
-    select(sample, chr, position, reads) %>% # ignore strand
-    distinct() %>%
-    filter(reads >= as.integer(args$reads)) %>%
     GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE,
         start.field="position", end.field="position")
+
+#TODO:
+# function: for each chromosome, count TTAA and AATT; also count per gene
+# use on all chrs
+genome = seq$genome("GRCm38")
+# subset default chromosomes
+# construct GRanges with all TTAA sites
+# count sites in genes and use as gene length
+ttaa = Biostrings::matchPattern("TTAA", genome$`1`)
+aatt = Biostrings::matchPattern("AATT", genome$`1`)
+# count each region only once per sample?
 
 #stretch(anchor_3p(genes), as.integer(args$upstream)) #TODO: check if this works
 #stretch(anchor_5p(genes), as.integer(args$downstream))
