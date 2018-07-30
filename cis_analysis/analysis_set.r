2baf5ecc4cc07a166755e9a3cf879bfa87e11f74
mschubert <mschu.dev@gmail.com>
2018-07-30 08:34:20 +0000

    adjust sample ids

--- cis_analysis/analysis_set.r
+++ cis_analysis/analysis_set.r
@@ -7,10 +7,11 @@ args = sys$cmd$parse(
     opt('r', 'reads', 'min reads to consider ins', '20'),
     opt('o', 'outfile', 'filtered samples & positions', 'analysis_set.RData'))
 
-#TODO: read this from sample sheet
+#TODO: read this from sample sheet (?)
 exclude = c(
     "401s", # using t
     "408s", # ctl
+    "410s", # only one transposon; use uLN instead
     "414s", # ctl
     "418s", # using t
     "422t", # using s
@@ -31,21 +32,27 @@ exclude = c(
     "610s",
     "612s", # using t
     "613s", # using t
-    "621s", # ???
-    "628s", # ???
-    "629s", # ???
+    NULL
+)
+
+replace = c(
+    "1jspbpb" = "145s",
+    "2jspbpb" = "155s",
+    "3jspbpb" = "157s",
+    "4jspbpb" = "180s",
+    "8jspbpb" = "184s",
+    "23jspbpb" = "212s",
+    "410uln" = "410s",
     NULL
 )
 
 # s&t different tumors: 184, 443 (RNA also both)
-# missing: 145s, 155s, 157s, 180s, 184s, 212s, 410s
-
 ins = io$load(args$infile) %>%
     dplyr::select(sample, chr, position, reads) %>% # ignore strand
     distinct() %>%
-    mutate(sample = ifelse(sample == "410uln", "410s", sample)) %>%
-    filter(!sample %in% exclude,
-           grepl("[0-9]{3}[st]", sample),
+    filter(!sample %in% exclude) %>%
+    mutate(sample = ifelse(sample %in% names(replace), replace[sample], sample)) %>%
+    filter(grepl("[0-9]{3}[st]", sample),
            reads >= as.integer(args$reads)) %>%
     arrange(sample, chr, position)
 
