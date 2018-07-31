4db30d351fde59416b6de5fdd94e992c5d2ae7a5
mschubert <mschu.dev@gmail.com>
2018-07-31 11:36:12 +0000

    fix upstream ext

--- cis_analysis/poisson.r
+++ cis_analysis/poisson.r
@@ -6,7 +6,7 @@ sys = import('sys')
 
 args = sys$cmd$parse(
     opt('i', 'infile', 'transposon insert RData', 'analysis_set.RData'),
-    opt('u', 'upstream', 'bp to include before gene', '1000'), #TOOD: 10k (?)
+    opt('u', 'upstream', 'bp to include before gene', '10000'),
     opt('d', 'downstream', 'bp to include after gene', '0'),
     opt('o', 'outfile', 'insertion statistics RData', 'poisson.RData'))
 
@@ -21,11 +21,11 @@ n_smp = length(unique(ins$sample))
 ins_per_sample = length(ins) / n_smp
 ins_rate = ins_per_sample / ins_sites_total
 
-#stretch(anchor_3p(genes), as.integer(args$upstream)) #TODO: check if this works
-#stretch(anchor_5p(genes), as.integer(args$downstream))
 genes = seq$coords$gene(dset="mmusculus_gene_ensembl", granges=TRUE) %>%
     filter(seqnames(.) %in% seqnames(ins)) %>%
-    select(external_gene_name) %>% # ...stretch here...
+    select(external_gene_name) %>%
+    anchor_3p() %>% stretch(as.integer(args$upstream)) %>%
+    anchor_5p() %>% stretch(as.integer(args$downstream)) %>%
     mutate(TTAAs = seq$count_pattern("TTAA", genome, ., rc=TRUE))
 
 samples = seq$intersect(ins, genes) %>%
