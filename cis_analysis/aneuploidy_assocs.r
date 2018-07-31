3e125f35484ccf8f9fe9783314d9d9fcb21d4734
mschubert <mschu.dev@gmail.com>
2018-07-31 06:58:10 +0000

    move aneup analysis set to compare_ploidy

--- cis_analysis/aneuploidy_assocs.r
+++ cis_analysis/aneuploidy_assocs.r
@@ -23,25 +23,15 @@ ks_test = function(data) {
 }
 
 args = sys$cmd$parse(
-    opt('a', 'aneup', 'aneuploidy .tsv', '../ploidy_compare/compare_ploidy.tsv'),
+    opt('a', 'aneup', 'aneuploidy .tsv', '../ploidy_compare/analysis_set.RData'),
     opt('p', 'poisson', 'cis assocs RData', 'poisson.RData'),
     opt('o', 'outfile', 'aneuploidy assocs', 'aneuploidy_assocs.RData'))
 
+aneup = io$load(args$aneup)
 dset = io$load(args$poisson)
 genes = dset$result %>%
     filter(adj.p < 1e-5) %>%
     pull(external_gene_name)
-
-# load and merge aneuploidy scores
-aneup = io$read_table(args$aneup, header=TRUE) %>%
-    select(-coverage, -tissue) %>%
-    filter(!duplicated(data.frame(type, sample)),
-           !sample %in% c("S", "T")) %>%
-    mutate(sample = fix_ids(sample)) %>%
-    tidyr::spread("type", "aneuploidy") %>%
-    group_by(sample) %>%
-    summarize(aneup = `WGS (merged)` %or% `WGS (30-cell)` %or% `RNA-seq (eT)`) %>%
-    arrange(-aneup)
 
 result = dset$samples %>%
     select(-n_ins) %>%
--- ploidy_compare/analysis_set.r
+++ ploidy_compare/analysis_set.r
@@ -15,7 +15,7 @@ args = sys$cmd$parse(
     opt('o', 'outfile', 'aneuploidy assocs', 'analysis_set.RData'))
 
 #TODO: track which data source we use to determine aneuploidy
-aneup = io$read_table(args$aneup, header=TRUE) %>%
+aneup = io$read_table(args$infile, header=TRUE) %>%
     select(-coverage, -tissue) %>%
     filter(!duplicated(data.frame(type, sample)),
            !sample %in% c("S", "T")) %>%
