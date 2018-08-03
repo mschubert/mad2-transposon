5b755e375f0cf74c396538223e8abddbf13127d8
mschubert <mschu.dev@gmail.com>
2018-08-03 10:02:15 +0000

    add cis; graph communities

--- diff_expr/genenet.r
+++ diff_expr/genenet.r
@@ -14,20 +14,39 @@ args = sys$cmd$parse(
     opt('p', 'plotfile', 'pdf', 'aneup_de.pdf'))
 
 aneup = io$load(args$aneup)
-cis = io$load(args$cis)
-is_cis = cis$result %>% filter(adj.p < 1e-3) %>% pull(external_gene_name) %>% unique()
+
+# adding this to network means 13 fewer samples, no assocs fdr<.25 w/ aneup
+ins = io$load(args$cis)
+is_cis = ins$result %>%
+    filter(adj.p < 1e-6 | external_gene_name %in%
+           c("Xrcc6", "Rapgef6", "Bcl11a", "Eed", "Suz12", "Arid2")) %>%
+    pull(external_gene_name) %>%
+    unique()
+cis = ins$samples %>%
+    filter(external_gene_name %in% is_cis) %>%
+    narray::construct(n_ins ~ sample + external_gene_name, fill=0)
+cis = t((!is.na(cis) & cis!=0) + 0)
+rownames(cis) = paste("cis", rownames(cis), sep="_")
+
+keep_gene = seq$coords$gene(dset="mmusculus_gene_ensembl", chromosomes=c(1:19,'X')) %>%
+    pull(external_gene_name)
 
 eset = io$load(args$expr) # filter on read count, variance?
 rownames(eset$expr) = eset$gene
-expr = eset$expr[rowMeans(eset$counts >= 10) >= 0.1 | eset$gene %in% cis,]
+expr = eset$expr[eset$gene %in% keep_gene &
+                 (rowMeans(eset$counts >= 10) >= 0.1 | eset$gene %in% cis),]
 idx = eset$idx %>% transmute(sample=paste0(hist_nr, tissue), tissue, type)
-narray::intersect(expr, idx$sample, aneup$sample, along=2)
+narray::intersect(expr, idx$sample, aneup$sample, along=2) # cis: -13 samples
 mat = rbind(aneup=aneup$aneup, expr)
 
 # add IS as nodes?
 
 # infer genenet
-pcor = gnet$pcor(mat, fdr=0.1)
+pcor = gnet$pcor(mat, fdr=0.25) # Ets1, Erg, aneup connected over 0.21 w/o CIS in net [aneup nothing with]
+
+g = igraph::graph_from_data_frame(pcor, directed=FALSE) %>%
+    as_tbl_graph() %>%
+    tidygraph::mutate(community = as.factor(group_infomap()))
 
 # subset w/ aneup+CIS cor [or only CIS for non-aneup related]
 # plot this subset
