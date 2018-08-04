a8cac2febdb0f90d6f19a057dbc183e6c27743a6
mschubert <mschu.dev@gmail.com>
2018-08-04 13:52:48 +0000

    make importable

--- diff_expr/aneup_de.r
+++ diff_expr/aneup_de.r
@@ -28,46 +28,49 @@ plot_volcano = function(res, coef, highlight=NULL) {
                                   dset="mmusculus_gene_ensembl"),
                circle = label %in% highlight,
                size = log10(baseMean + 1)) %>%
-        plt$p_effect("pvalue", "log2FoldChange", thresh=0.01) %>%
-        plt$volcano(base.size=5, label_top=30, repel=TRUE) + ggtitle(coef)
+        plt$p_effect("padj", "log2FoldChange", thresh=0.1) %>%
+        plt$volcano(p=0.1, base.size=5, label_top=30, repel=TRUE) + ggtitle(coef)
 }
 
-args = sys$cmd$parse(
-    opt('e', 'expr', 'gene expression RData', '../data/rnaseq/assemble.RData'),
-    opt('c', 'copies', 'gene copy matrix', '../ploidy_compare/gene_copies.RData'),
-    opt('a', 'aneup', 'aneuploidy score', '../ploidy_compare/analysis_set.RData'),
-    opt('i', 'cis', 'cis site RData', '../cis_analysis/poisson.RData'),
-    opt('o', 'outfile', 'results RData', 'aneup_de.RData'),
-    opt('p', 'plotfile', 'pdf', 'aneup_de.pdf'))
+sys$run({
+    args = sys$cmd$parse(
+        opt('e', 'expr', 'gene expression RData', '../data/rnaseq/assemble.RData'),
+        opt('c', 'copies', 'gene copy matrix', '../ploidy_compare/gene_copies.RData'),
+        opt('a', 'aneup', 'aneuploidy score', '../ploidy_compare/analysis_set.RData'),
+        opt('i', 'cis', 'cis site RData', '../cis_analysis/poisson.RData'),
+        opt('o', 'outfile', 'results RData', 'aneup_de.RData'),
+        opt('p', 'plotfile', 'pdf', 'aneup_de.pdf'))
 
-cis_genes = io$load(args$cis)$result %>%
-    filter(adj.p < 1e-5) %>%
-    pull(external_gene_name)
-gene_copies = io$load(args$copies)
-exprset = io$load(args$expr)
-idx = exprset$idx %>%
-    mutate(sample = paste0(hist_nr, tissue),
-           type = sub("(^[^ ]+).*", "\\1", type)) %>%
-    left_join(io$load(args$aneup)) %>%
-    mutate(type = ifelse(is.na(type), "NA", type), #TODO: add annotations
-           tissue = factor(tissue), type=factor(type))
-counts = exprset$counts
-narray::intersect(gene_copies, counts, along=1)
-narray::intersect(gene_copies, counts, idx$sample, along=2)
+    cis = io$load(args$cis)$result
+    cis_genes = cis %>% filter(adj.p < 1e-3) %>% pull(external_gene_name)
+    gene_copies = io$load(args$copies)
+    exprset = io$load(args$expr)
+    idx = exprset$idx %>%
+        mutate(sample = paste0(hist_nr, tissue),
+               type = sub("(^[^ ]+).*", "\\1", type)) %>%
+        left_join(io$load(args$aneup)) %>%
+        mutate(type = ifelse(is.na(type), "NA", type), #TODO: add annotations
+               tissue = factor(tissue), type=factor(type))
+    counts = exprset$counts
+    narray::intersect(gene_copies, counts, along=1)
+    narray::intersect(gene_copies, counts, idx$sample, along=2)
 
-# vst w/ copy num corr
-eset = DESeq2::DESeqDataSetFromMatrix(counts, colData=idx, ~tissue) %>%
-    DESeq2::estimateSizeFactors(normMatrix=gene_copies)
-vs = DESeq2::getVarianceStabilizedData(eset)
-design(eset) = ~ tissue + type + aneup
-res = DESeq2::DESeq(eset) #, test="LRT", full=~tissue+type, reduced=~tissue)
-DESeq2::resultsNames(res)
+    # vst w/ copy num corr
+    eset = DESeq2::DESeqDataSetFromMatrix(counts, colData=idx, ~tissue) %>%
+        DESeq2::estimateSizeFactors(normMatrix=gene_copies)
+    vs = DESeq2::getVarianceStabilizedData(eset)
+    design(eset) = ~ tissue + type + aneup
+    res = DESeq2::DESeq(eset) #, test="LRT", full=~tissue+type, reduced=~tissue)
+    DESeq2::resultsNames(res)
 
-pdf(args$plotfile)
-pca = prcomp(t(vs[apply(vs, 1, var) > 0,]), center=TRUE, scale=FALSE)
-plot_pcs(idx, pca, 1, 2)
-plot_pcs(idx, pca, 3, 4)
-plot_pcs(idx, pca, 5, 6)
-for (name in setdiff(DESeq2::resultsNames(res), "Intercept"))
-    print(plot_volcano(res, name, cis_genes))
-dev.off()
+    pdf(args$plotfile)
+    pca = prcomp(t(vs[apply(vs, 1, var) > 0,]), center=TRUE, scale=FALSE)
+    plot_pcs(idx, pca, 1, 2)
+    plot_pcs(idx, pca, 3, 4)
+    plot_pcs(idx, pca, 5, 6)
+    for (name in setdiff(DESeq2::resultsNames(res), "Intercept"))
+        print(plot_volcano(res, name, cis_genes))
+    dev.off()
+
+    save(eset, pca, cis, res, file=args$outfile)
+})
