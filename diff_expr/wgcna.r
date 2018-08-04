64dc4ce65c135dd5f188ea5e87efdf4f8b89e514
mschubert <mschu.dev@gmail.com>
2018-08-04 10:29:59 +0000

    remove genes w/o name

--- diff_expr/wgcna.r
+++ diff_expr/wgcna.r
@@ -100,5 +100,5 @@ print(gnet$plot_pcor_net(gnet$pcor(t(both)), pval=0.05, node_text=4, edge_text=2
 
 dev.off()
 
-genes = unstack(data.frame(colnames(expr), merged$colors))
+genes = unstack(na.omit(data.frame(colnames(expr), merged$colors)))
 save(merged, genes, MEs, file=args$outfile)
