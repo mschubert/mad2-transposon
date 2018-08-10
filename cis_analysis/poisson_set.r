cac8195b3f69b7d906b738b2b80a2d8688d72a55
mschubert <mschu.dev@gmail.com>
2018-08-10 19:21:29 +0000

    include sample table

--- cis_analysis/poisson_set.r
+++ cis_analysis/poisson_set.r
@@ -2,23 +2,34 @@ library(dplyr)
 io = import('io')
 sys = import('sys')
 gset = import('data/genesets')
+plt = import('plot')
 
 test_set = function(set_name) {
     subs = stats$external_gene_name %in% sets[[set_name]]
     len = sum(stats$TTAAs[subs])
     ins = sum(stats$n_smp[subs])
-    broom::tidy(poisson.test(ins, len*assocs$n_smp, assocs$ins_rate_genome))
+    smps = names(unlist(stats$sample[subs]))
+    broom::tidy(poisson.test(ins, len*assocs$n_smp, assocs$ins_rate_genome)) %>%
+        mutate(size = sum(subs),
+               n_smps = length(unique(smps)),
+               samples = list(sample=smps))
 }
 
 args = sys$cmd$parse(
-    opt('g', 'gene', 'gene-level poisson', 'poisson.RData'),
-    opt('s', 'set', 'gene set file', '{set}.RData'),
-    opt('o', 'outfile', 'save results .RData', 'poisson_{set}.RData'))
+    opt('i', 'infile', 'gene-level poisson', 'poisson.RData'),
+#    opt('s', 'set', 'gene set file', '{set}.RData'),
+    opt('o', 'outfile', 'save results .RData', 'poisson_set.RData'),
+    opt('p', 'plotfile', 'pdf', 'poisson_set.pdf'))
 
-assocs = io$load(args$gene)
+assocs = io$load(args$infile)
+cis_genes = assocs$result %>%
+    filter(adj.p < 1e-5) %>%
+    pull(external_gene_name)
 ins = assocs$samples %>%
     group_by(external_gene_name) %>%
-    summarize(n_smp = sum(n_ins > 0))
+    summarize(n_smp = sum(n_ins > 0),
+              sample = list(unique(sample))) %>%
+    filter(!external_gene_name %in% cis_genes)
 stats = as.data.frame(assocs$gene) %>%
     select(external_gene_name, TTAAs) %>%
     inner_join(ins)
@@ -29,7 +40,19 @@ sets = gset$go("mmusculus_gene_ensembl", "external_gene_name", as_list=TRUE) %>%
 result = sapply(names(sets), test_set, simplify=FALSE) %>%
     dplyr::bind_rows(.id="set") %>%
     select(-(parameter:alternative)) %>%
+    select(set, size, n_smps, everything()) %>%
     mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
     arrange(adj.p, p.value)
 
-save(result, file=args$outfile)
+samples = result %>%
+    select(set, samples) %>%
+    tidyr::unnest() %>%
+    group_by(samples, set) %>%
+    summarize(n_ins = n()) %>%
+    ungroup() %>%
+    dplyr::rename(sample = samples)
+
+result = result %>%
+    select(-samples)
+
+save(result, samples, file=args$outfile)
