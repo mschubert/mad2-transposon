77f386c5c7f234b3030c3f0047c7ce2056ca99b8
mschubert <mschu.dev@gmail.com>
2018-08-15 14:24:51 +0000

    refactor eT_ploidy

--- ploidy_from_rnaseq/eT_ploidy.r
+++ ploidy_from_rnaseq/eT_ploidy.r
@@ -1,63 +1,74 @@
-library(reshape2)
 library(dplyr)
-library(ggplot2)
-library(ggbio)
-library(GenomicRanges)
 io = import('io')
 seq = import('seq')
+sys = import('sys')
 
-ref = io$load("../data/rnaseq/Mad2+p53_batch2.RData")$counts[,c("eT_p0", "eT_p2")]
-ctl = edgeR::cpm(ref)
-
-load('../data/rnaseq/assemble.RData') # counts, expr, idx
-cpm = edgeR::cpm(counts)
-narray::intersect(ctl, ref, cpm, counts, along=1)
-keep = narray::map(cbind(ctl, cpm), along=2, function(x) all(x > 0.5 & x < 1000))
-ctl = edgeR::cpm(ref[keep,])
-cpm = edgeR::cpm(counts[keep,])
-scaled = (2 * cpm / narray::crep(rowMeans(ctl), ncol(cpm))) %>%
-    as.data.frame() %>%
-    tibble::rownames_to_column("ensembl_gene_id")
-
-genes = seq$coords$gene("ensembl_gene_id", dset="mmusculus_gene_ensembl", granges=TRUE) %>%
-    plyranges::select(ensembl_gene_id) %>%
-    as.data.frame() %>%
-    dplyr::inner_join(scaled) %>%
-    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE)
-genome = seq$genome("GRCm38")
-seqinfo(genes) = seqinfo(genome)
-
-extract_segment = function(data) {
-    density_modal = function(x, bw=1) {
-        x = log2(x[x>0.5])
-        den = density(x, kernel="gaussian", bw=bw)
-        2^den$x[den$y==max(den$y)]
-    }
-    ediv = ecp::e.divisive(as.matrix(data$expr))
-    data$clust = ediv$cluster
-    data %>%
-        group_by(clust) %>%
-        summarize(end = max(start),
-                  start = min(start),
-                  expr = density_modal(expr)) %>%
-        select(-clust)
+#' Return normalized read counts for a count matrix
+normalize_reads = function(mat) {
+    idx = data.frame(id=colnames(mat))
+    DESeq2::DESeqDataSetFromMatrix(mat, colData=idx, ~1) %>%
+        DESeq2::estimateSizeFactors() %>%
+        DESeq2::counts(normalized=TRUE)
 }
-genes = as.data.frame(genes) %>%
-    filter(seqnames %in% c(1:19,'X')) %>%
-    arrange(seqnames, start) %>%
-    tidyr::gather("sample", "expr", -(seqnames:ensembl_gene_id)) %>%
-    mutate(sample = sub("^X", "", sample)) %>%
-    tbl_df()
 
-segments = genes %>%
-    group_by(seqnames, sample) %>%
-    tidyr::nest() %>%
-    mutate(result = purrr::map(data, extract_segment)) %>%
-    select(-data) %>%
-    tidyr::unnest() %>%
-    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE) %>%
-    as.data.frame() %>%
-    mutate(sample = sub("^X", "", sample)) %>%
-    tbl_df()
+extract_segment = function(smp, chr, ratio, genes) {
+    mat = ratio[genes$seqnames == chr, smp, drop=FALSE]
+    ediv = ecp::e.divisive(mat)
 
-save(segments, genes, file="eT_ploidy.RData")
+    res = cbind(genes[genes$seqnames == chr,], mat=mat, cluster=ediv$cluster) %>%
+        group_by(cluster) %>%
+        summarize(start = min(start),
+                  end = max(end),
+                  expr = median(mat)) %>%
+        select(-cluster)
+
+#    density_modal = function(x, bw=1) {
+#        x = log2(x[x>0.5])
+#        den = density(x, kernel="gaussian", bw=bw)
+#        2^den$x[den$y==max(den$y)]
+#    }
+#    ediv = ecp::e.divisive(as.matrix(data$expr))
+#    data$clust = ediv$cluster
+#    data %>%
+#        group_by(clust) %>%
+#        summarize(end = max(start),
+#                  start = min(start),
+#                  expr = density_modal(expr)) %>%
+#        select(-clust)
+}
+
+sys$run({
+    args = sys$cmd$parse(
+        opt('r', 'ref', 'eT expression RData', '../data/rnaseq/Mad2+p53_batch2.RData'),
+        opt('e', 'expr', 'expression RData', '../data/rnaseq/assemble.RData'),
+        opt('o', 'outfile', 'results RData', 'eT_ploidy.RData'))
+
+    genes = seq$coords$gene("ensembl_gene_id", dset="mmusculus_gene_ensembl", granges=TRUE) %>%
+        plyranges::select(ensembl_gene_id) %>%
+        as.data.frame() %>%
+        filter(seqnames %in% c(1:19, 'X'))
+
+    smps = c("eT_p0", "eT_p2")
+    ref = io$load(args$ref)$counts[,smps]
+    panel = io$load(args$expr)$counts
+    eset = normalize_reads(narray::stack(ref, panel, along=2))
+    ref = eset[,colnames(ref)]
+    panel = eset[,colnames(panel)]
+
+    keep_fun = function(x) mean(x > 50 & x < 1e4) >= 0.75
+    keep_ref = narray::map(ref, along=2, keep_fun)
+    keep_panel = narray::map(panel, along=2, keep_fun)
+    keep = keep_ref[keep_ref & keep_panel]
+
+    narray::intersect(ref, panel, keep, genes$ensembl_gene_id, along=1)
+    ratio = panel / rowMeans(ref)
+
+    segments = expand.grid(sample=colnames(ratio), seqnames=c(1:19,'X'),
+                           stringsAsFactors=FALSE) %>%
+        mutate(result = purrr::map2(sample, seqnames, extract_segment,
+                                    genes=genes, ratio=ratio)) %>%
+        select(-data) %>%
+        tidyr::unnest()
+
+    save(segments, genes, file="eT_ploidy.RData")
+})
