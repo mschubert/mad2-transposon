4e75e1c77d1a8fe00a0eb7388a60428b7f4d6aab
mschubert <mschu.dev@gmail.com>
2018-08-16 09:45:28 +0000

    drop ks stat

--- cis_analysis/aneup_assocs.r
+++ cis_analysis/aneup_assocs.r
@@ -4,18 +4,6 @@ io = import('io')
 sys = import('sys')
 plt = import('plot')
 
-#' KS statistic
-ks_test = function(data, field) {
-    mod = ks.test(data[[field]][data$reads], data[[field]][!data$reads])
-    mod$model = select(data, sample, !!rlang::sym(field), reads)
-    mod %>%
-        broom::tidy() %>%
-        mutate(estimate = mean(data[[field]][data$reads]) - mean(data[[field]]),
-               statistic = sign(estimate) / p.value,
-               size = sum(data$reads),
-               mod = list(mod))
-}
-
 #' Poisson regression
 poisson_reg = function(data, field) {
     fml = formula(paste("reads ~", field))
@@ -30,14 +18,11 @@ poisson_reg = function(data, field) {
 }
 
 #' Run given test for a field
-do_test = function(dset, test, field) {
-    test_fun = switch(args$test, 'ks'=ks_test, 'poisson'=poisson_reg,
-                      stop("invalid 'test' argument"))
-
+do_test = function(dset, field) {
     dset %>%
         group_by(external_gene_name) %>%
         tidyr::nest() %>%
-        mutate(fit = purrr::map(data, test_fun, field=field)) %>%
+        mutate(fit = purrr::map(data, poisson_reg, field=field)) %>%
         select(-data) %>%
         tidyr::unnest() %>%
         mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
@@ -45,21 +30,20 @@ do_test = function(dset, test, field) {
         arrange(adj.p, p.value)
 }
 
-plot_volcano = function(aset, field) {
-    res = do_test(aset, args$test, field)
+plot_volcano = function(res) {
     res %>%
         mutate(label = external_gene_name) %>%
         plt$p_effect(thresh=0.1) %>%
-        plt$volcano(p=0.1, label_top=30, repel=TRUE) + ggtitle(field)
+        plt$volcano(p=0.1, label_top=30, repel=TRUE) +
+            ylab("p-value (non-adj.)")
 }
 
 sys$run({
     args = sys$cmd$parse(
         opt('a', 'aneup', 'aneuploidy .tsv', '../ploidy_compare/analysis_set.RData'),
         opt('p', 'poisson', 'cis assocs RData', 'poisson.RData'),
-        opt('t', 'test', 'ks/poisson', 'ks'),
-        opt('o', 'outfile', 'aneuploidy assocs', 'aneup_assocs/ks.RData'),
-        opt('p', 'plotfile', 'pdf', 'aneup_assocs/ks.pdf'))
+        opt('o', 'outfile', 'aneuploidy assocs', 'aneup_assocs/gene.RData'),
+        opt('p', 'plotfile', 'pdf', 'aneup_assocs/gene.pdf'))
 
     aneup = io$load(args$aneup)
     dset = io$load(args$poisson)
@@ -82,14 +66,14 @@ sys$run({
 
     fields = c("aneuploidy", "type_Tcell", "type_Myeloid", "type_Other",
                "aneup_Tcell", "aneup_Myeloid", "aneup_Other")
-    plots = lapply(fields, plot_volcano, aset=aset)
+    results = sapply(fields, do_test, dset=aset, simplify=FALSE)
 
     #TODO: use formulas in poisson regression, allow covars (and use type for aneup:type)
 
     pdf(args$plotfile)
-    for (p in plots)
-        print(p)
+    for (rn in names(results))
+        print(plot_volcano(results[[rn]]) + ggtitle(rn))
     dev.off()
 
-    save(genes, file=args$outfile) # rather save result
+    save(results, file=args$outfile)
 })
