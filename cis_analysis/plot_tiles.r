26f48d91b896720431acc4cbd50b2896f81e68e5
mschubert <mschu.dev@gmail.com>
2018-08-16 16:15:55 +0000

    refactor plot_tiles

--- cis_analysis/plot_tiles.r
+++ cis_analysis/plot_tiles.r
@@ -6,79 +6,96 @@ library(patchwork)
 io = import('io')
 sys = import('sys')
 
-args = sys$cmd$parse(
-    opt('a', 'aneup', 'sample-level aneup scores', '../ploidy_compare/analysis_set.RData'),
-    opt('i', 'ins_dna', 'all DNA insertions table', 'poisson.RData'),
-    opt('j', 'ins_rna', 'all RNA insertions table', '../data/rnaseq_imfusion/insertions.txt'),
-    opt('d', 'assocs_dna', 'CIS in DNA', 'aneup_assocs.RData'),
-    opt('r', 'assocs_rna', 'CTG in RNA', '../data/rnaseq_imfusion/merged_ctgs.txt'),
-    opt('e', 'exons', 'exon expression table', '../data/rnaseq_imfusion/exon_counts.txt'),
-    opt('n', 'n_dna', 'min number samples with ins', '5'),
-    opt('m', 'n_rna', 'min number samples with ins', '2'),
-    opt('p', 'plotfile', 'pdf to plot to', 'plot_tiles.pdf'))
+plot_dna_tiles = function(cis_samples, cis_result, aneup_assocs, n_dna) {
+    genes = cis_result %>%
+        filter(adj.p < 1e-5,
+               n_smp >= as.integer(n_dna)) %>%
+        inner_join(aneup_assocs %>% select(external_gene_name, ast=statistic)) %>%
+        arrange(ast) %>%
+        pull(external_gene_name)
 
-expr = io$read_table(args$exons, header=TRUE)
+    dna_tiles = cis_samples %>%
+        transmute(sample = sample,
+                  gene_name = factor(external_gene_name, levels=genes),
+                  ins = 1) %>%
+        na.omit() %>%
+        tidyr::complete(sample, gene_name, fill=list(ins=0))
 
-aneup = io$load(args$aneup) %>%
-    arrange(aneuploidy) %>%
-    mutate(sample = factor(sample, levels=sample))
+    plot_tiles(dna_tiles) +
+        theme(legend.position = "left") +
+        ggtitle(paste("CIS min", n_dna, "samples, 20 reads"))
+}
 
-cis = io$load(args$ins_dna)
-genes = cis$result %>%
-    filter(adj.p < 1e-5,
-           n_smp >= as.integer(args$n_dna)) %>%
-    pull(external_gene_name) %>%
-    unique()
-dna = io$load(args$assocs_dna)$aneuploidy %>%
-    arrange(statistic)
-dna_tiles = cis$samples %>%
-    filter(external_gene_name %in% dna$external_gene_name) %>%
-    transmute(sample = factor(sample, levels=levels(aneup$sample)),
-              gene_name = factor(external_gene_name, levels=genes),
-              ins = 1) %>%
-    na.omit() %>%
-    tidyr::complete(sample, gene_name, fill=list(ins=0))
-left = ggplot(dna_tiles, aes(x=gene_name, y=sample)) +
-    geom_tile(aes(fill=ins), color="white") +
-    coord_fixed() +
-    viridis::scale_fill_viridis(option="magma", direction=-1) +
-    theme(axis.text.x = element_text(size=10, angle=65, hjust=1),
-          axis.text.y = element_text(size=10),
-          axis.title.x = element_text(size=12),
-          legend.position = "left") +
-    ggtitle(paste("CIS min", args$n_dna, "samples, 20 reads"))
+plot_rna_tiles = function(ins_rna, ctg, n_rna) {
+    rna_tiles = ins_rna %>%
+        select(sample, gene_name) %>%
+        mutate(ins = 1) %>%
+        tidyr::complete(sample, gene_name, fill=list(ins=0)) %>%
+        filter(gene_name %in% ctg$gene_name) %>%
+        na.omit() %>%
+        tidyr::complete(sample, gene_name, fill=list(ins=NA))
 
-ins_rna = io$read_table(args$ins_rna, header=TRUE)
-ctg = io$read_table(args$assocs_rna, header=TRUE) %>%
-    filter(n_samples > as.integer(args$n_rna))
-rna_tiles = ins_rna %>%
-    transmute(sample = factor(sample, levels=grep("^[0-9]+[st]$", colnames(expr), value=TRUE)), # all RNA
-              gene_name = gene_name,
-              ins = 1) %>%
-    tidyr::complete(sample, gene_name, fill=list(ins=0)) %>%
-    filter(gene_name %in% ctg$gene_name) %>%
-    mutate(sample = factor(sample, levels=levels(aneup$sample))) %>%
-    na.omit() %>%
-    tidyr::complete(sample, gene_name, fill=list(ins=NA))
-mid = ggplot(rna_tiles, aes(x=gene_name, y=sample)) +
-    geom_tile(aes(fill=ins), color="white") +
-    coord_fixed() +
-    viridis::scale_fill_viridis(option="magma", direction=-1, na.value="transparent") +
-    theme(axis.text.x = element_text(size=10, angle=65, hjust=1),
-          axis.text.y = element_blank(),
-          axis.title.y = element_blank(),
-          axis.title.x = element_text(size=12)) +
-    guides(fill=FALSE) +
-    ggtitle(paste("CTGs min", args$n_rna, "samples"))
+    plot_tiles(rna_tiles) +
+        theme(axis.text.y = element_blank(),
+              axis.title.y = element_blank()) +
+        guides(fill=FALSE) +
+        ggtitle(paste("CTGs min", n_rna, "samples"))
+}
 
-right = ggplot(aneup, aes(x=aneuploidy, y=sample)) +
-    geom_segment(aes(xend=aneuploidy, yend=sample), x=0, color="lightgrey") +
-    geom_point() +
-    theme(axis.text.x = element_text(size=10),
-          axis.title.x = element_text(size=12),
-          axis.text.y = element_blank(),
-          axis.title.y = element_blank())
+plot_tiles = function(data) {
+    ggplot(data, aes(x=gene_name, y=sample)) +
+        geom_tile(aes(fill=ins), color="white") +
+        coord_fixed() +
+        viridis::scale_fill_viridis(option="magma", direction=-1) +
+        theme(axis.text.x = element_text(size=10, angle=65, hjust=1),
+              axis.text.y = element_text(size=10),
+              axis.title.x = element_text(size=12))
+}
 
-pdf(args$plotfile, 18, 15)
-print(left + mid + right + plot_layout(nrow=1, widths=c(3,1,1)))
-dev.off()
+plot_aneup = function(aneup) {
+    ggplot(aneup, aes(x=aneuploidy, y=sample)) +
+        geom_segment(aes(xend=aneuploidy, yend=sample), x=0, color="lightgrey") +
+        geom_point() +
+        theme(axis.text.x = element_text(size=10),
+              axis.title.x = element_text(size=12),
+              axis.text.y = element_blank(),
+              axis.title.y = element_blank())
+}
+
+sys$run({
+    args = sys$cmd$parse(
+        opt('a', 'aneup', 'sample-level aneup scores', '../ploidy_compare/analysis_set.RData'),
+        opt('i', 'ins_dna', 'all DNA insertions table', 'poisson.RData'),
+        opt('j', 'ins_rna', 'all RNA insertions table', '../data/rnaseq_imfusion/insertions.txt'),
+        opt('d', 'assocs_dna', 'CIS in DNA', 'aneup_assocs.RData'),
+        opt('r', 'assocs_rna', 'CTG in RNA', '../data/rnaseq_imfusion/merged_ctgs.txt'),
+        opt('e', 'exons', 'exon expression table', '../data/rnaseq_imfusion/exon_counts.txt'),
+        opt('n', 'n_dna', 'min number samples with ins', '5'),
+        opt('m', 'n_rna', 'min number samples with ins', '2'),
+        opt('p', 'plotfile', 'pdf to plot to', 'plot_tiles.pdf'))
+
+    aneup_assocs = io$load(args$assocs_dna)$aneuploidy
+    aneup = io$load(args$aneup) %>%
+        arrange(aneuploidy) %>%
+        mutate(sample = factor(sample, levels=sample))
+
+    cis = io$load(args$ins_dna)
+    cis_result = cis$result
+    cis_samples = cis$samples %>%
+        mutate(sample = factor(sample, levels=levels(aneup$sample)))
+
+    expr = io$read_table(args$exons, header=TRUE)
+    ins_rna = io$read_table(args$ins_rna, header=TRUE) %>%
+        mutate(sample = factor(sample, levels=grep("^[0-9]+[st]$", colnames(expr), value=TRUE)),
+               sample = factor(sample, levels=levels(aneup$sample)))
+    ctg = io$read_table(args$assocs_rna, header=TRUE) %>%
+        filter(n_samples >= as.integer(args$n_rna))
+
+    left = plot_dna_tiles(cis_samples, cis_result, aneup_assocs, args$n_dna)
+    mid = plot_rna_tiles(ins_rna, ctg, args$n_rna)
+    right = plot_aneup(aneup)
+
+    pdf(args$plotfile, 16, 14)
+    print(left + mid + right + plot_layout(nrow=1, widths=c(5,2,2)))
+    dev.off()
+})
