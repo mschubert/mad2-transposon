6590ee108b0fd761693240d96b7d603315f3d951
mschubert <mschu.dev@gmail.com>
2018-08-18 09:00:59 +0000

    fit sample rate with each external assoc

--- cis_analysis/aneup_assocs.r
+++ cis_analysis/aneup_assocs.r
@@ -4,32 +4,36 @@ io = import('io')
 sys = import('sys')
 plt = import('plot')
 
-#' Poisson regression
-poisson_reg = function(data, field) {
-    fml = formula(paste("reads ~", field))
-    mod = glm(fml, family=poisson(), data=data)
-    mod$model = select(data, sample, !!rlang::sym(field), reads)
-    mod %>%
-        broom::tidy() %>%
-        filter(term == field) %>%
-        select(-term) %>%
-        mutate(size = sum(data$reads),
-               mod = list(mod))
+#' Test different insertion statistics of a gene with an external variable
+#'
+#' This currently does not correct for when a cancer type is both more aneuploid
+#' and has more insertions in a given gene.
+#'
+#' @param dset  data.frame with fields: sample, gene, ext_var[, subset]
+#' @param gene  external_gene_name to test
+#' @param ext_var  differential insertion rate with what, e.g. 'aneuploidy'
+#' @param is_type  filter dset with specific 'type' (default: all)
+#' @return  data.frame with tidy association results
+test_gene = function(dset, gene, ext_var, is_type=NA) {
+    `%>%` = magrittr::`%>%`
+    if (is.na(is_type))
+        is_type = unique(dset$type)
+    tset = dset %>%
+        dplyr::filter(type %in% is_type) %>%
+        dplyr::mutate(is_gene = external_gene_name == gene,
+                      ext = !! rlang::sym(ext_var)) %>%
+        dplyr::select(reads, sample, is_gene, ext)
+    mod = glm(reads ~ 0 + sample + is_gene * ext, family=poisson(), data=tset)
+    broom::tidy(mod) %>%
+        dplyr::mutate(size = sum(tset$is_gene)) %>%
+        dplyr::filter(term == "is_geneTRUE:ext") %>%
+        dplyr::select(-term)
 }
 
-#' Run given test for a field
-do_test = function(dset, field) {
-    dset %>%
-        group_by(external_gene_name) %>%
-        tidyr::nest() %>%
-        mutate(fit = purrr::map(data, poisson_reg, field=field)) %>%
-        select(-data) %>%
-        tidyr::unnest() %>%
-        mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
-        select(external_gene_name, size, estimate, statistic, p.value, adj.p) %>%
-        arrange(adj.p, p.value)
-}
-
+#' Volcano plot
+#'
+#' @param res  association result data.frame: external_gene_name, p.value, estimate
+#' @return  ggplot2 object
 plot_volcano = function(res) {
     res %>%
         mutate(label = external_gene_name) %>%
@@ -40,35 +44,36 @@ plot_volcano = function(res) {
 
 sys$run({
     args = sys$cmd$parse(
-        opt('a', 'aneup', 'aneuploidy .tsv', '../ploidy_compare/analysis_set.RData'),
+        opt('m', 'meta', 'meta+aneuploidy', '../ploidy_compare/analysis_set.RData'),
         opt('p', 'poisson', 'cis assocs RData', 'poisson.RData'),
-        opt('o', 'outfile', 'aneuploidy assocs', 'aneup_assocs/gene.RData'),
-        opt('p', 'plotfile', 'pdf', 'aneup_assocs/gene.pdf'))
+        opt('o', 'outfile', 'aneuploidy assocs', 'aneup_assocs.RData'),
+        opt('p', 'plotfile', 'pdf', 'aneup_assocs.pdf'))
 
-    aneup = io$load(args$aneup)
+    meta = io$load(args$meta)
     dset = io$load(args$poisson)
-    genes = dset$result %>%
-        filter(adj.p < 1e-5) %>%
-        pull(external_gene_name)
+    genes = with(dset, intersect(samples$external_gene_name,
+                                 result$external_gene_name))
 
     aset = dset$samples %>%
         select(-n_ins) %>%
         filter(external_gene_name %in% genes) %>%
         mutate(reads=TRUE) %>%
         tidyr::complete(sample, external_gene_name, fill=list(reads=FALSE)) %>%
-        inner_join(aneup, by="sample") %>%
-        mutate(type_Tcell = ifelse(type == "T-cell", 1, 0),
-               type_Myeloid = ifelse(type == "Myeloid", 1, 0),
-               type_Other = ifelse(type == "Other", 1, 0),
-               aneup_Tcell = ifelse(type == "T-cell", aneuploidy, 0),
-               aneup_Myeloid = ifelse(type == "Myeloid", aneuploidy, 0),
-               aneup_Other = ifelse(type == "Other", aneuploidy, 0))
+        inner_join(meta, by="sample")
 
-    fields = c("aneuploidy", "type_Tcell", "type_Myeloid", "type_Other",
-               "aneup_Tcell", "aneup_Myeloid", "aneup_Other")
-    results = sapply(fields, do_test, dset=aset, simplify=FALSE)
+    fields = c("aneuploidy", "T-cell", "Myeloid", "Other")
+    result = expand.grid(external_gene_name=genes, ext=fields,
+            type=unique(aset$type), stringsAsFactors=FALSE) %>%
+        filter(is.na(type) | ext == "aneuploidy") %>%
+        mutate(res = clustermq::Q(test_gene, const=list(dset=aset),
+            gene=external_gene_name, ext_var=ext, is_type=type,
+            job_size=5, n_jobs=200, memory=5120)) %>%
+        tidyr::unnest()
 
-    #TODO: use formulas in poisson regression, allow covars (and use type for aneup:type)
+    results = result %>%
+        mutate(subset = ifelse(is.na(type), ext, paste0(type, ":", ext))) %>%
+        select(-ext, -type) %>%
+        split(.$subset)
 
     pdf(args$plotfile)
     for (rn in names(results))
