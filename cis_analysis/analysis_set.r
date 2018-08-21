d82e6701076c34f94e4e2eb4fb9f5bf86bf42d2c
mschubert <mschu.dev@gmail.com>
2018-08-21 09:35:42 +0000

    move exclude/rename to yaml

--- cis_analysis/analysis_set.r
+++ cis_analysis/analysis_set.r
@@ -5,53 +5,18 @@ sys = import('sys')
 args = sys$cmd$parse(
     opt('i', 'infile', 'all samples .RData', '../data/cis/cis_per_tumor.RData'),
     opt('r', 'reads', 'min reads to consider ins', '20'),
+    opt('s', 'sheet', 'exclude/replace yaml', 'analysis_set.yaml'),
     opt('o', 'outfile', 'filtered samples & positions', 'analysis_set.RData'))
 
-#TODO: read this from sample sheet (?)
-exclude = c(
-    "401s", # using t
-    "408s", # ctl
-    "410s", # only one transposon; use uLN instead
-    "414s", # ctl
-    "418s", # using t
-    "422t", # using s
-    "426s", # ctl
-    "430s", # ctl
-    "436s", # ctl
-    "438s", # ctl
-    "439s", # ctl
-    "441s", # ctl
-    "445s", # ctl
-    "446t", # using s
-    "452s", # using t
-    "460s", # ctl
-    "475s", # ctl
-    "477s", # using t
-    "478s", # control
-    "480s", # control
-    "610s",
-    "612s", # using t
-    "613s", # using t
-    NULL
-)
-
-replace = c(
-    "1jspbpb" = "145s",
-    "2jspbpb" = "155s",
-    "3jspbpb" = "157s",
-    "4jspbpb" = "180s",
-    "8jspbpb" = "184s",
-    "23jspbpb" = "212s",
-    "410uln" = "410s",
-    NULL
-)
+sheet = io$read_yaml(args$sheet)
 
 # s&t different tumors: 184, 443 (RNA also both)
 ins = io$load(args$infile) %>%
     dplyr::select(sample, chr, position, reads) %>% # ignore strand
     distinct() %>%
-    filter(!sample %in% exclude) %>%
-    mutate(sample = ifelse(sample %in% names(replace), replace[sample], sample)) %>%
+    filter(!sample %in% names(sheet$exclude)) %>%
+    mutate(sample = ifelse(sample %in% names(sheet$replace),
+                           unlist(sheet$replace)[sample], sample)) %>%
     filter(grepl("[0-9]{3}[st]", sample),
            reads >= as.integer(args$reads)) %>%
     arrange(sample, chr, position)
