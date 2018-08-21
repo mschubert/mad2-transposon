38b729b25ce34369e61e60d745363d67f160995f
mschubert <mschu.dev@gmail.com>
2018-08-21 10:11:37 +0000

    include if likely local hop (#12)

--- cis_analysis/analysis_set.r
+++ cis_analysis/analysis_set.r
@@ -2,10 +2,25 @@ library(dplyr)
 io = import('io')
 sys = import('sys')
 
+#' Local hopping correction by maximum number of reads in a window
+#'
+#' @param pos  Integer vector with positions
+#' @param reads  number of reads
+#' @param wsize  window size
+#' @return  TRUE/FALSE if position is considered a local hop
+is_local_hop = function(pos, reads, wsize) {
+    cur = seq_along(pos)
+    before = c(1, seq_along(pos)[-length(pos)])
+    after = c(seq_along(pos)[-1], length(pos))
+    (reads[cur] < reads[before] & abs(pos[cur] - pos[before]) < wsize) |
+        (reads[cur] < reads[after] & abs(pos[cur] - pos[after]) < wsize)
+}
+
 args = sys$cmd$parse(
     opt('i', 'infile', 'all samples .RData', '../data/cis/cis_per_tumor.RData'),
     opt('r', 'reads', 'min reads to consider ins', '20'),
     opt('s', 'sheet', 'exclude/replace yaml', 'analysis_set.yaml'),
+    opt('w', 'wsize', 'window size local hops', '50000'),
     opt('o', 'outfile', 'filtered samples & positions', 'analysis_set.RData'))
 
 sheet = io$read_yaml(args$sheet)
@@ -19,6 +34,9 @@ ins = io$load(args$infile) %>%
                            unlist(sheet$replace)[sample], sample)) %>%
     filter(grepl("[0-9]{3}[st]", sample),
            reads >= as.integer(args$reads)) %>%
-    arrange(sample, chr, position)
+    arrange(sample, chr, position) %>%
+    group_by(sample, chr) %>%
+    mutate(is_local = is_local_hop(position, reads, as.integer(args$wsize))) %>%
+    ungroup()
 
 save(ins, file=args$outfile)
