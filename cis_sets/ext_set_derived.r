cc4129b507df6ec87add069dcf9b42d23230e430
mschubert <mschu.dev@gmail.com>
2018-08-24 15:44:40 +0000

    make it explicit that assocs derived

--- cis_sets/ext_set_derived.r
+++ cis_sets/ext_set_derived.r
@@ -33,22 +33,23 @@ plot_volcano = function(res) {
     res %>%
         mutate(label = set_name) %>%
         plt$p_effect("adj.p", thresh=0.1) %>%
-        plt$volcano(p=0.1, label_top=30, base.size=0.2, text.size=2, repel=TRUE)
+        plt$volcano(p=0.1, label_top=30, text.size=2, repel=TRUE)
 }
 
 sys$run({
     args = sys$cmd$parse(
         opt('m', 'meta', 'meta+aneuploidy', '../ploidy_compare/analysis_set.RData'),
-        opt('g', 'gene', 'cis assocs RData', '../cis_analysis/ext_gene.RData'),
-        opt('s', 'set', 'cis for sets', 'poisson_set.RData'),
-        opt('o', 'outfile', 'aneuploidy assocs', 'ext_set.RData'),
-        opt('p', 'plotfile', 'pdf', 'ext_set.pdf'))
+        opt('c', 'cis_gene', 'poisson cis', '../cis_analysis/poisson.RData'), # ignored
+        opt('g', 'ext_gene', 'cis assocs RData', '../cis_analysis/ext_gene.RData'),
+        opt('s', 'cis_set', 'cis for sets', 'poisson_set.RData'),
+        opt('o', 'outfile', 'aneuploidy assocs', 'ext_set_derived.RData'),
+        opt('p', 'plotfile', 'pdf', 'ext_set_derived.pdf'))
 
     meta = io$load(args$meta) %>%
         mutate(aneuploidy = pmin(aneuploidy, 0.2))
-    cis_sets = io$load(args$set)$result %>%
+    cis_sets = io$load(args$cis_set)$result %>%
         filter(adj.p < 0.05) %>% pull(set)
-    dset = io$load(args$gene)
+    dset = io$load(args$ext_gene)
     go = gset$go('mmusculus_gene_ensembl', 'external_gene_name', as_list=TRUE)
     go = go[cis_sets]
 
