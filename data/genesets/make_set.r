89eac132f4779c1e928af9adb71d967a353dea23
mschubert <mschu.dev@gmail.com>
2018-08-29 09:42:24 +0000

    map mouse orthologue gene names

--- data/genesets/make_set.r
+++ data/genesets/make_set.r
@@ -1,10 +1,11 @@
 sys = import('sys')
+idmap = import('process/idmap')
 enr = import('tools/enrichr')
 msdb = import('tools/msigdb')
 
 args = sys$cmd$parse(
-    opt('g', 'geneset', 'Identifier of the gene set'),
-    opt('o', 'outfile', 'File to save gene set to'))
+    opt('g', 'geneset', 'Identifier of the gene set', 'KEA_2015'),
+    opt('o', 'outfile', 'File to save gene set to', '/dev/null'))
 
 if (args$geneset %in% enr$dbs()$name)
     sets = enr$genes(args$geneset)
@@ -13,4 +14,8 @@ else if (args$geneset %in% msdb$dbs())
 else
     stop("invalid gene set: ", args$geneset)
 
-save(sets, file=args$outfile)
+mouse = stack(sets)
+mouse$values = unname(idmap$orthologue(mouse$values, from="external_gene_name", to="mgi_symbol"))
+mouse = unstack(na.omit(mouse))
+
+save(mouse, file=args$outfile)
