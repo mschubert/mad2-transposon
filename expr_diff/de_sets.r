68e9cf6c9bb365ccdbe0c44a5dc05e0d811f3180
mschubert <mschu.dev@gmail.com>
2018-08-29 09:59:43 +0000

    diff expr for all sets separately

--- expr_diff/de_sets.r
+++ expr_diff/de_sets.r
@@ -10,7 +10,7 @@ gset = import('data/genesets')
 
 plot_gset = function(res, sets, highlight=NULL) {
     test_one = function(set_name) {
-        fdata = mutate(cur, in_set = ensembl_gene_id %in% sets[[set_name]])
+        fdata = mutate(cur, in_set = gene_name %in% sets[[set_name]])
         mod = try(lm(stat ~ in_set, data=fdata))
         if (class(mod) == "try-error")
             return()
@@ -19,32 +19,33 @@ plot_gset = function(res, sets, highlight=NULL) {
             select(-term) %>%
             mutate(size = sum(fdata$in_set, na.rm=TRUE))
     }
-    cur = res %>% mutate(stat = log2FoldChange / lfcSE)
+    cur = res %>%
+        mutate(stat = log2FoldChange / lfcSE,
+               gene_name = idmap$gene(ensembl_gene_id,
+                    to="external_gene_name", dset="mmusculus_gene_ensembl"))
     result = sapply(names(sets), test_one, simplify=FALSE) %>%
         dplyr::bind_rows(.id="label") %>%
         mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
         arrange(adj.p, p.value)
     result %>%
-        plt$p_effect(thresh=0.1) %>%
+        plt$p_effect("adj.p", thresh=0.1) %>%
         plt$volcano(p=0.1, base.size=0.5, label_top=30, repel=TRUE, text.size=2)
 }
 
 sys$run({
     args = sys$cmd$parse(
         opt('e', 'de', 'diff expr RData', 'de.RData'),
-#        opt('o', 'outfile', 'results RData', 'de_sets.RData'),
+        opt('s', 'sets', 'gene set RData', '../data/genesets/GO_Biological_Process_2017b.RData'),
         opt('p', 'plotfile', 'pdf', 'de_sets.pdf'))
 
     res = io$load(args$de)$res
-    go = gset$go('mmusculus_gene_ensembl', 'ensembl_gene_id', as_list=TRUE) %>%
+    sets = io$load(args$sets) %>%
         gset$filter(min=5, max=200, valid=rownames(counts))
 
     pdf(args$plotfile)
     for (name in names(res)) {
         message(name)
-        print(plot_gset(res[[name]], go) + ggtitle(name))
+        print(plot_gset(res[[name]], sets) + ggtitle(name))
     }
     dev.off()
-
-#    save(eset, pca, cis, res, file=args$outfile)
 })
