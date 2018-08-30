3a58784cb007ae1e8e910f044440683009ae4d73
mschubert <mschu.dev@gmail.com>
2018-08-30 13:07:07 +0000

    use set objs

--- cis_sets/ext_set.r
+++ cis_sets/ext_set.r
@@ -24,14 +24,17 @@ test_set = function(dset, set_name, ext_var, is_type=NA, sets) {
         dplyr::filter(type %in% is_type) %>%
         dplyr::mutate(in_set = external_gene_name %in% sets[[set_name]],
                       ext = !! rlang::sym(ext_var))
-    # try 1|sample, 1|type
-    mod = lme4::glmer(reads ~ ext + ext:in_set + (1|external_gene_name),
-                      family=poisson(), data=tset)
+    n_ins = sum(with(tset, reads[in_set]))
+    if (n_ins < 5)
+        return()
+    mod6 = lme4::glmer(reads ~ ext + ext:in_set + (1|sample) + 
+        (ext|type) + (1|external_gene_name), family=poisson(), data=tset,
+        control=lme4::glmerControl(optCtrl=list(method="L-BFGS-B")))
     broom::tidy(mod) %>%
         dplyr::filter(term == "ext:in_setTRUE") %>%
         dplyr::mutate(n_genes = length(sets[[set_name]]),
-                      ins_set = sum(with(tset, reads[in_set]))) %>%
-        dplyr::select(n_genes, ins_set, estimate:p.value)
+                      n_ins = n_ins) %>%
+        dplyr::select(n_genes, n_ins, estimate:p.value)
 }
 
 #' Volcano plot
@@ -52,13 +55,13 @@ sys$run({
         opt('g', 'ext_gene', 'cis assocs RData', '../cis_analysis/ext_gene.RData'), # ignored
         opt('s', 'cis_set', 'cis for sets', 'poisson_set.RData'), # ignored
         opt('f', 'sets', 'RData for gene set', '../data/genesets/KEA_2015.RData'),
-        opt('o', 'outfile', 'aneuploidy assocs', 'ext_set.RData'),
-        opt('p', 'plotfile', 'pdf', 'ext_set.pdf'))
+        opt('o', 'outfile', 'aneuploidy assocs', 'ext_set/KEA_2015.RData'),
+        opt('p', 'plotfile', 'pdf', 'ext_set/KEA_2015.pdf'))
 
     meta = io$load(args$meta) %>%
         mutate(aneuploidy = pmin(aneuploidy, 0.2))
     dset = io$load(args$cis_gene)
-    go = gset$go('mmusculus_gene_ensembl', 'external_gene_name', as_list=TRUE) %>%
+    sets = io$load(args$sets)
         gset$filter(min=5, max=200, valid=dset$result$external_gene_name)
 
     aset = dset$samples %>%
@@ -69,17 +72,17 @@ sys$run({
 
     # 100 @ 20 jobs = 30 min; 25k @ 200 (x25) -> 12 hours
     fields = c("aneuploidy", "T-cell", "Myeloid", "Other")
-    result = expand.grid(set_name=names(go), ext=fields,
+    result = expand.grid(set_name=names(sets), ext=fields,
             type=unique(aset$type), stringsAsFactors=FALSE) %>%
         filter(is.na(type) | ext == "aneuploidy") %>%
-        mutate(res = clustermq::Q(test_set, const=list(dset=aset, sets=go),
+        mutate(res = clustermq::Q(test_set, const=list(dset=aset, sets=sets),
             set_name=set_name, ext_var=ext, is_type=type,
             job_size=5, n_jobs=300, memory=3072)) %>%
         tidyr::unnest()
 
     results = res %>%
         mutate(label = ifelse(is.na(type), ext, paste(type, ext, sep=":")),
-               size = ins_set,
+               size = n_ins,
                adj.p = p.adjust(p.value, method="fdr"))
     results = split(results, results$label)
 
