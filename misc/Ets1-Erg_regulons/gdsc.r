d4b7f56bbf0362bf6be510e7823938b590e76fa2
mschubert <mschu.dev@gmail.com>
2018-09-04 08:43:38 +0000

    use MI regulons

--- misc/Ets1-Erg_regulons/gdsc.r
+++ misc/Ets1-Erg_regulons/gdsc.r
@@ -1,7 +1,9 @@
 library(cowplot)
 library(dplyr)
+io = import('io')
 gdsc = import('data/gdsc')
 enr = import('tools/enrichr')
+plt = import('plot')
 
 keep = gdsc$tissues(c("LAML", "DLBC", "LCML", "ALL"))
 expr = gdsc$basal_expression()
@@ -10,11 +12,14 @@ narray::intersect(keep, expr, aneup$cosmic, along=-1)
 
 # find human blood ets, erg binding in enrichr
 tfs = enr$genes("ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X")
-scores = GSVA::gsva(expr, tfs)
-
-idx = cbind(aneup, tissue=unname(keep),
-            ERG_expr=expr["ERG",], ETS1_expr=expr["ETS1",],
-            ERG_reg=scores["ERG_CHEA",], ETS1_reg=scores["ETS1_ENCODE",])
+mi = io$load("../../data/networks/E-GEOD-13159.RData") %>%
+    select(Target, Regulator) %>%
+    unstack()
+regs = c(tfs[c('ETS1_ENCODE', 'ERG_CHEA')], mi[c('ETS1', 'ERG')])
+names(regs) = c("ETS1_chip", "ERG_chip", "ETS1_mi", "ERG_mi")
+scores = GSVA::gsva(expr, regs)
+idx = cbind(aneup, tissue=unname(keep), t(scores),
+            ERG_expr=expr["ERG",], ETS1_expr=expr["ETS1",])
 
 pdf("gdsc.pdf")
 ggplot(idx, aes(x=ERG_expr, y=ETS1_expr)) +
@@ -24,19 +29,27 @@ ggplot(idx, aes(x=ERG_expr, y=ETS1_expr)) +
     geom_text_repel(aes(label=cell_line), size=2) +
     ggtitle("TF expression")
 
-ggplot(idx, aes(x=ERG_reg, y=ETS1_reg)) +
+plt$venn(regs)
+
+ggplot(idx, aes(x=ERG_chip, y=ETS1_chip)) +
     geom_point(aes(size=aneuploidy, fill=tissue), shape=21) +
     geom_text_repel(aes(label=cell_line), size=2) +
-    ggtitle("Regulon enrichment")
+    ggtitle("ChIP regulon enrichment")
 
 idx2 = idx %>%
     filter(ERG_expr > 3.5 & ETS1_expr > 3.5, tissue == "ALL")
 idx2$aneup_class = ifelse(idx2$aneuploidy > 0.5, "aneuploid", "euploid")
-p = lm(ETS1_reg ~ aneup_class:ERG_reg, data=idx2) %>%
+p = lm(ETS1_chip ~ aneup_class:ERG_chip, data=idx2) %>%
     broom::tidy() %>% pull(p.value)
-ggplot(idx2, aes(x=ERG_reg, y=ETS1_reg, fill=aneup_class)) +
+ggplot(idx2, aes(x=ERG_chip, y=ETS1_chip, fill=aneup_class)) +
     geom_point(aes(size=aneuploidy), shape=21) +
     geom_smooth(aes(color=aneup_class), method="lm", se=FALSE) +
     geom_text_repel(aes(label=cell_line), size=2) +
     ggtitle(sprintf("ALL regulon enrichment, TF expressed (p eup %.2f, anp %.2f)", p[3], p[2]))
+
+ggplot(idx, aes(x=ERG_mi, y=ETS1_mi)) +
+    geom_point(aes(size=aneuploidy, fill=tissue), shape=21) +
+    geom_text_repel(aes(label=cell_line), size=2) +
+    ggtitle("MI regulon enrichment")
+
 dev.off()
--- misc/Ets1-Erg_regulons/mad2pb.r
+++ misc/Ets1-Erg_regulons/mad2pb.r
@@ -1,24 +1,32 @@
 library(cowplot)
 library(dplyr)
 io = import('io')
+gdsc = import('data/gdsc')
 enr = import('tools/enrichr')
 idmap = import('process/idmap')
+plt = import('plot')
+
+clean_reg = function(x) x[!is.na(x) & !duplicated(x)]
 
 dset = io$load("../../data/rnaseq/assemble.RData")
 expr = dset$expr
 rownames(expr) = dset$genes
-meta = io$load("../../ploidy_compare/analysis_set.RData")
+meta = io$load("../../ploidy_compare/analysis_set.RData") %>%
+    select(sample, aneuploidy, type)
 narray::intersect(expr, meta$sample, along=-1)
 
 # find human blood ets, erg binding in enrichr
-tfs = stack(enr$genes("ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X"))
-tfs$values = unname(idmap$orthologue(tfs$values, from="external_gene_name", to="mgi_symbol"))
-tfs = unstack(tfs)
-scores = GSVA::gsva(expr, tfs)
-
-idx = cbind(meta %>% select(sample, aneuploidy, type),
-            Erg_expr=expr["Erg",], Ets1_expr=expr["Ets1",],
-            Erg_reg=scores["ERG_CHEA",], Ets1_reg=scores["ETS1_ENCODE",])
+tfs = enr$genes("ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X")
+mi = io$load("../../data/networks/E-GEOD-13159.RData") %>%
+    select(Target, Regulator) %>%
+    unstack()
+regs = c(tfs[c('ETS1_ENCODE', 'ERG_CHEA')], mi[c('ETS1', 'ERG')]) %>%
+    idmap$orthologue(from="external_gene_name", to="mgi_symbol") %>%
+    lapply(clean_reg)
+names(regs) = c("ETS1_chip", "ERG_chip", "ETS1_mi", "ERG_mi")
+scores = GSVA::gsva(expr, regs)
+idx = cbind(meta, t(scores),
+            Erg_expr=expr["Erg",], Ets1_expr=expr["Ets1",])
 
 pdf("mad2pb.pdf")
 ggplot(idx, aes(x=Erg_expr, y=Ets1_expr)) +
@@ -26,9 +34,16 @@ ggplot(idx, aes(x=Erg_expr, y=Ets1_expr)) +
     ggrepel::geom_text_repel(aes(label=sample), size=2) +
     ggtitle("TF expression")
 
-ggplot(idx, aes(x=Erg_reg, y=Ets1_reg)) +
+plt$venn(regs)
+
+ggplot(idx, aes(x=ERG_chip, y=ETS1_chip)) +
     geom_point(aes(size=aneuploidy, fill=type), shape=21) +
-    ggrepel::geom_text_repel(aes(label=sample), size=2) +
-    ggtitle("Regulon enrichment")
+    geom_text_repel(aes(label=sample), size=2) +
+    ggtitle("ChIP regulon enrichment")
+
+ggplot(idx, aes(x=ERG_mi, y=ETS1_mi)) +
+    geom_point(aes(size=aneuploidy, fill=type), shape=21) +
+    geom_text_repel(aes(label=sample), size=2) +
+    ggtitle("MI regulon enrichment")
 
 dev.off()
