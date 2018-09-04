bda34c52e1e765e446ca0f8b3931c90b31020fb6
mschubert <mschu.dev@gmail.com>
2018-09-04 12:59:15 +0000

    gene set overlap stub

--- link_cis_expr/gene_overlap.r
+++ link_cis_expr/gene_overlap.r
@@ -1,1 +1,16 @@
-# load sets, compute scores
+io = import('io')
+sys = import('sys')
+plt = import('plot')
+
+args = sys$cmd$parse(
+    opt('s', 'select', 'yaml which sets to use', './interesting_sets.yaml'),
+    opt('p', 'plotfile', 'pdf', 'gene_overlap.pdf'),
+    arg('genesets', 'RData files', arity='*',
+        list.files("../data/genesets", "\\.RData$", full.names=TRUE))
+)
+
+select = io$read_yaml(args$select)
+sets = io$load(args$genesets)
+
+pdf(args$plotfile)
+dev.off()
