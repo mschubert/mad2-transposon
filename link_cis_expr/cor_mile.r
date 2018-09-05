dfc4f2e8814bfd814ddeef3e57a67bdf4fdb7de7
mschubert <mschu.dev@gmail.com>
2018-09-05 14:47:36 +0000

    plot per subtype

--- link_cis_expr/cor_mile.r
+++ link_cis_expr/cor_mile.r
@@ -10,12 +10,17 @@ args = sys$cmd$parse(
         list.files("gsva_mile", "\\.RData$", full.names=TRUE))
 )
 
-expr = Biobase::exprs(io$load(args$expr))[c("ENSG00000134954", "ENSG00000157554"),]
+dset = io$load(args$expr)
+types = Biobase::pData(dset)$FactorValue..LEUKEMIA.CLASS.
+expr = Biobase::exprs(dset)[c("ENSG00000134954", "ENSG00000157554"),]
 rownames(expr) = c("ETS1", "ERG")
+
 select = io$read_yaml(args$select)$expr_sets
 sets = io$load(args$genesets)
 sets = lapply(names(select), function(s) sets[[s]][select[[s]],,drop=FALSE])
+
 mat = narray::stack(c(sets, list(expr)), along=1)
+tmat = narray::split(mat, along=2, subsets=types)
 
 pm = util$pcor(t(mat))
 
@@ -23,4 +28,10 @@ pdf(args$plotfile, 20, 15)
 util$plot_cor_matrix(t(mat), text_color=NULL)
 util$plot_pcor_net(pm, node_size=4, edge_size=1)
 util$plot_bootstrapped_pcor(t(mat), node_size=4)
+
+for (i in seq_along(tmat)) {
+    name = names(tmat)[i]
+    util$plot_cor_matrix(t(tmat[[i]]), title=name, text_color=NULL)
+    try(util$plot_bootstrapped_pcor(t(tmat[[i]]), fdr=0.3, node_size=4, title=name))
+}
 dev.off()
