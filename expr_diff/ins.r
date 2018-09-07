9fe4e268475f900ba2cf24047da6955dfd3ed0ee
mschubert <mschu.dev@gmail.com>
2018-09-07 16:10:25 +0000

    include network plot for inserts

--- expr_diff/ins.r
+++ expr_diff/ins.r
@@ -1,18 +1,29 @@
 library(DESeq2)
 library(ggplot2)
 library(dplyr)
+library(tidygraph)
 io = import('io')
 sys = import('sys')
 de = import('./de')
 set = import('./de_sets')
+vp = import('../link_cis_expr/cor_viper')
+idmap = import('process/idmap')
 
 args = sys$cmd$parse(
     opt('d', 'diff_expr', 'gene expression RData', 'de.RData'),
     opt('i', 'ins', 'gene name of insert', 'Erg'),
+    opt('n', 'network', 'aracne', '../data/networks/E-GEOD-13159.RData'),
     opt('o', 'outfile', 'results RData', 'ins/Erg.RData'),
     opt('p', 'plotfile', 'pdf', 'ins/Erg.pdf'),
     arg('sets', 'gene set .RData', arity='*',
         list.files("../data/genesets", "\\.RData", full.names=TRUE)))
+
+om = function(x) idmap$orthologue(x, from="external_gene_name", to="mgi_symbol")
+net = io$load(args$network) %>%
+    mutate(Regulator = unname(om(Regulator)),
+           Target = unname(om(Target))) %>%
+    na.omit()
+tf_net = filter(net, Target %in% Regulator)
 
 dset = io$load(args$diff_expr)
 cis = dset$cis$samples %>%
@@ -25,11 +36,15 @@ idx = colData(eset) %>%
     mutate(ins = ifelse(is.na(ins), 0, 1))
 eset@colData = DataFrame(idx)
 
+expr = assay(eset)
+rownames(expr) = idmap$gene(rownames(expr),
+    to="external_gene_name", dset="mmusculus_gene_ensembl")
+
 design(eset) = ~ tissue + type + ins
 res = DESeq2::estimateDispersions(eset) %>%
     DESeq2::nbinomWaldTest(maxit=1000) %>%
     de$extract_coef("ins") %>%
-    mutate(gene_name = de$idmap$gene(ensembl_gene_id, from="ensembl_gene_id",
+    mutate(gene_name = idmap$gene(ensembl_gene_id, from="ensembl_gene_id",
         to="external_gene_name", dset="mmusculus_gene_ensembl"))
 
 sets = io$load(args$sets) %>%
@@ -37,8 +52,17 @@ sets = io$load(args$sets) %>%
     lapply(function(x) set$gset$filter(x, min=5, valid=na.omit(res$gene_name)))
 
 pdf(args$plotfile)
+
 print(de$plot_pcs(idx, dset$pca, 1, 2, hl=cis$sample))
 print(de$plot_volcano(res) + ggtitle("ins"))
+
+dviper = vp$diff_viper(expr, net, eset$ins)
+dcor = vp$diff_cor(expr, tf_net, eset$ins)
+p = try(vp$plot_subnet(dviper, dcor))
+if (class(p) == "try-error" || class(try(ggplot_build(p))) == "try-error")
+    p = ggplot(data.frame()) + geom_point() + xlim(0, 10) + ylim(0, 100)
+print(p + ggtitle("MI network"))
+
 for (name in names(sets))
     print(set$plot_gset(res, sets[[name]]) + ggtitle(name))
 dev.off()
