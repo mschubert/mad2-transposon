de147e621798bfff8bc6fd3450c6d95ae1e2d931
mschubert <mschu.dev@gmail.com>
2018-10-01 14:53:57 +0000

    split regulons in up/down

--- data/genesets/mi_regulons.r
+++ data/genesets/mi_regulons.r
@@ -1,10 +1,29 @@
+library(dplyr)
+b = import('base')
 io = import('io')
 sys = import('sys')
+st = import('stats')
+idmap = import('process/idmap')
+aracne = import('tools/aracne')
 
 args = sys$cmd$parse(
+    opt('e', 'expr', 'expression RData', '../arrayexpress/E-GEOD-13159.RData'),
     opt('n', 'network', 'aracne results RData', '../networks/E-GEOD-13159.RData'),
     opt('o', 'outfile', 'gene set RData', 'MI_regulons.RData'))
 
-regs = unstack(io$load(args$network)[c(2,1)])
+expr = Biobase::exprs(io$load(args$expr))
+rownames(expr) = idmap$gene(rownames(expr), to="hgnc_symbol")
+expr = expr[!is.na(rownames(expr)),]
+
+get_cor = function(r, t) cor(expr[r,], expr[t,]) %catch% NA
+
+regs = io$load(args$network) %>%
+    mutate(cor = purrr::map2_dbl(Regulator, Target, get_cor),
+           z = st$cor$fisher_r2z(cor),
+           sign = ifelse(z > 0, "up", "down")) %>%
+    filter(abs(z) > 0.5) %>%
+    mutate(Regulator = paste(Regulator, sign, sep="_"))
+
+regs = unstack(regs[c("Target", "Regulator")])
 
 save(regs, file=args$outfile)
