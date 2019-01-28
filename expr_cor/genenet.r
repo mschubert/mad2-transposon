daece623893c1940cf6587ebbe09757a164b8c9f
mschubert <mschu.dev@gmail.com>
2019-01-28 13:53:52 +0000

    adjust new dsets

--- expr_cor/genenet.r
+++ expr_cor/genenet.r
@@ -96,64 +96,17 @@ plot_pcor_table = function(pm, field="aneuploidy") {
 
 sys$run({
     args = sys$cmd$parse(
-        opt('m', 'meta', 'sample metadata', '../ploidy_compare/analysis_set.RData'), # only mad2pb
-        opt('s', 'select', 'yaml', 'interesting_sets.yaml'),
         opt('e', 'expr', 'expr RData', '../data/rnaseq/assemble.RData'),
-        opt('p', 'plotfile', 'pdf', 'genenet_mad2pb.pdf'),
-        arg('genesets', 'RData files', arity='*',
-            list.files("../expr_sets/gsva_mad2pb", "\\.RData$", full.names=TRUE))
+        opt('p', 'plotfile', 'pdf', 'genenet_mad2pb.pdf')
     )
 
     dset = io$load(args$expr)
-
-    if (grepl("rnaseq/assemble.RData", args$expr)) {
-        types = dset$idx$type
-        expr = dset$expr[match(c("Ets1", "Erg"), dset$genes),]
-        rownames(expr) = c("Ets1", "Erg")
-        meta = io$load(args$meta)
-        aneup0.3 = pmin(meta$aneuploidy[match(colnames(expr), meta$sample)], 0.3)
-        expr = rbind(expr, aneup0.3)
-    } else if (grepl("E-GEOD", args$expr)) {
-        types = Biobase::pData(dset)$FactorValue..LEUKEMIA.CLASS.
-        expr = Biobase::exprs(dset)[c("ENSG00000134954", "ENSG00000157554"),]
-        rownames(expr) = c("ETS1", "ERG")
-    } else {
-        # copied from ../diff_expr/de_MILE.r
-        keep = !is.na(dset$meta$type)
-        expr = dset$expr[c("ETS1", "ERG"),keep]
-        types = cbind(narray::mask(dset$meta$type[keep]),
-            Hyperdip = (dset$meta$annot[keep] == "ALL with hyperdiploid karyotype")) + 0
-        types[,"Hyperdip"][types[,"B_like"] == 0] = NA
-#        aneuploidy = pmin(dset$meta$aneuploidy[keep], 0.25)
-    }
-
-    select = io$read_yaml(args$select)$expr_sets
-    sets = io$load(args$genesets)
-    sets = lapply(names(select), function(s) sets[[s]][select[[s]],,drop=FALSE])
-
-    mat = narray::stack(c(sets, list(expr)), along=1)[,colnames(expr)]
-    if (is.character(types)) {
-        tmat = narray::split(mat, along=2, subsets=types)
-        mat2 = rbind(mat, narray::mask(types, along=1) + 0)
-    } else { # mile2
-        tmat = sapply(colnames(types), function(t) {
-            mat[,!is.na(types[,t]) & types[,t] == 1]
-        }, simplify=FALSE)
-        mat2 = rbind(mat, t(types[,1:3]))
-    }
-
-    ecmp = lapply(tmat, reshape2::melt) %>%
-        dplyr::bind_rows(.id="type") %>%
-        dplyr::rename(set=Var1, sample=Var2, expr=value) %>%
-        group_by(set) %>%
-        mutate(z_expr = scale(expr)) %>%
-        ungroup()
+    tmat = sapply(colnames(types), function(t) {
+        mat[,!is.na(types[,t]) & types[,t] == 1]
+    }, simplify=FALSE)
+    mat2 = rbind(mat, t(types[,1:3]))
 
     pdf(args$plotfile, 20, 15)
-    p = ggplot(ecmp, aes(x=z_expr, y=set)) +
-        ggridges::geom_density_ridges(aes(fill=type), alpha=0.95)
-    print(p)
-
     plot_cor_matrix(t(mat2), text_color=NULL)
     pcor(t(mat)) %>% plot_pcor_net(node_size=4, edge_size=1)
     plot_bootstrapped_pcor(t(mat), node_size=4)
