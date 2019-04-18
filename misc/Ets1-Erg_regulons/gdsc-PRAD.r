8483bfe966edba424b2144493c26fdecb5a47c43
mschubert <mschu.dev@gmail.com>
2019-04-18 11:37:46 +0200

    add PRAD to compare Ets1/Erg

---
+++ misc/Ets1-Erg_regulons/gdsc-PRAD.r
@@ -0,0 +1,29 @@
+library(cowplot)
+library(dplyr)
+io = import('io')
+gdsc = import('data/gdsc')
+enr = import('tools/enrichr')
+plt = import('plot')
+sys = import('sys')
+
+args = sys$cmd$parse(
+    opt('p', 'plotfile', 'pdf', 'gdsc.pdf')
+)
+
+keep = gdsc$tissues(c("PRAD"))
+expr = gdsc$basal_expression()
+aneup = gdsc$aneuploidy()
+narray::intersect(keep, expr, aneup$cosmic, along=-1)
+
+# find human blood ets, erg binding in enrichr
+idx = cbind(aneup, tissue=unname(keep),
+            ERG_expr=expr["ERG",], ETS1_expr=expr["ETS1",])
+
+pdf(args$plotfile)
+ggplot(idx, aes(x=ERG_expr, y=ETS1_expr)) +
+    geom_hline(yintercept=3.5, linetype="dashed") +
+    geom_vline(xintercept=3.5, linetype="dashed") +
+    geom_point(aes(size=aneuploidy, fill=tissue), shape=21) +
+    geom_text_repel(aes(label=cell_line), size=2) +
+    ggtitle("TF expression")
+dev.off()
