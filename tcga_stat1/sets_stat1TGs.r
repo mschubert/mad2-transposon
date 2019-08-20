79d95bcb305436b10de0433a1bd491b5c24f84ec
mschubert <mschu.dev@gmail.com>
2019-08-20 22:48:16 +0200

    3k genes too many

--- tcga_stat1/sets_stat1TGs.r
+++ tcga_stat1/sets_stat1TGs.r
@@ -12,15 +12,16 @@ hm = io$load("../data/genesets/human/CH.HALLMARK.RData")
 msdb = io$load("../data/genesets/human/MSigDB_Oncogenic_Signatures.RData")
 reac = io$load("../data/genesets/human/Reactome_2016.RData")
 #mile = io$load("../data/genesets/human/MILE_regulons.RData")
-sets = c(go["regulation of complement activation (GO:0030449)"],
-         chea["STAT1_20625510_ChIP-Seq_HELA_Human"],
-         chea["STAT1_17558387_ChIP-Seq_HELA_Human"],
-         encc["STAT3_CHEA"],
-         encc["STAT3_ENCODE"],
-         hm["HALLMARK_INTERFERON_GAMMA_RESPONSE"],
+sets1 = c(go["regulation of complement activation (GO:0030449)"],
+          chea["STAT1_20625510_ChIP-Seq_HELA_Human"],
+          chea["STAT1_17558387_ChIP-Seq_HELA_Human"],
+          encc["STAT3_CHEA"],
+          encc["STAT3_ENCODE"]
+)
+sets2 = c(hm["HALLMARK_INTERFERON_GAMMA_RESPONSE"],
          reac["Interferon alpha/beta signaling_Homo sapiens_R−HSA−909733"]
 )
-
-genes = unique(unlist(sets, use.names=FALSE))
+genes = intersect(unlist(sets1, use.names=FALSE),
+                  unlist(sets2, use.names=FALSE))
 
 saveRDS(list(sets=list(), genes=genes), file=args$outfile)
