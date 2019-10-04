41e7721dcc50a64980303b8fb71e04973e26edfb
mschubert <mschu.dev@gmail.com>
2019-10-04 12:30:16 +0200

    PCA excl cgas, ifng as well

--- data/rnaseq_stat1/dset.r
+++ data/rnaseq_stat1/dset.r
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
+b = import('base')
 sys = import('sys')
+idmap = import('process/idmap')
 
 args = sys$cmd$parse(
     opt('y', 'yaml', 'yaml', 'BT549_Stat1.yaml'),
@@ -11,15 +13,16 @@ args = sys$cmd$parse(
 # map from yaml identifiers to count table identifiers
 experiment = yaml::read_yaml(args$yaml)
 samples = tibble(id=names(experiment$samples)) %>%
-    mutate(genotype = sub("^([^_]+)_.*", "\\1", id),
-           time = sub(".*_([0-9]+)[_-].*", "\\1", id),
-           treatment = sub(".*(dmso|rev|ifng|0).*", "\\1", id),
-           replicate = sapply(strsplit(id, "-"), function(x) x[[2]]))
+    mutate(genotype = relevel(factor(sub("^([^_]+)_.*", "\\1", id)), "wt"),
+           time = relevel(factor(sub(".*_([0-9]+)[_-].*", "\\1", id)), "0"),
+           treatment = relevel(factor(sub(".*(dmso|rev|ifng|0).*", "\\1", id)), "dmso"),
+           replicate = factor(sapply(strsplit(id, "-"), function(x) x[[2]])))
 
 # DESeq2 data set and size factors
 dset = readr::read_tsv(args$expr)
 counts = data.matrix(dset[-1])
 rownames(counts) = sub("\\.[0-9]+", "", dset$gene_id)
+rownames(counts) = idmap$gene(rownames(counts), to="external_gene_name") %or% rownames(counts)
 colnames(counts) = samples$id
 counts = counts[rowSums(counts) >= 10,]
 
@@ -32,6 +35,12 @@ DESeq2::plotPCA(vst, intgroup=c("genotype", "time")) +
     ggrepel::geom_text_repel(aes(label=name))
 
 DESeq2::plotPCA(vst[,!grepl("cgas_[^-]+-2", colnames(vst))], intgroup=c("genotype", "time")) +
+    ggrepel::geom_text_repel(aes(label=name))
+
+DESeq2::plotPCA(vst[,!grepl("cgas_.*", colnames(vst))], intgroup=c("genotype", "time")) +
+    ggrepel::geom_text_repel(aes(label=name))
+
+DESeq2::plotPCA(vst[,!grepl("cgas|ifng", colnames(vst))], intgroup=c("genotype", "time")) +
     ggrepel::geom_text_repel(aes(label=name))
 dev.off()
 
