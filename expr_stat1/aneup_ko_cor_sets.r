56210a794b2e306c956ec0b15fbe87155b49d15d
mschubert <mschu.dev@gmail.com>
2019-10-08 13:25:22 +0200

    aneup cor stubs

---
+++ expr_stat1/aneup_ko_cor_sets.r
@@ -0,0 +1,42 @@
+library(dplyr)
+io = import('io')
+sys = import('sys')
+idmap = import('process/idmap')
+gset = import('data/genesets')
+util = import('../expr_diff/util')
+
+args = sys$cmd$parse(
+    opt('d', 'diff_expr', 'rds', 'diff_expr.rds'),
+    opt('a', 'diff_aneup', 'RData', '../expr_diff/de_Mad2PB.RData'),
+    opt('h', 'set_human', 'RData', '../data/genesets/human/KEA_2015.RData'),
+    opt('m', 'set_mouse', 'RData', '../data/genesets/mouse/KEA_2015.RData'),
+    opt('p', 'plotfile', 'pdf', 'sets/KEA_2015.pdf'))
+
+stat1 = readRDS(args$diff_expr)
+aneup = io$load(args$diff_aneup)$aneuploidy
+aneup$gene_name = toupper(aneup$gene_name)
+#aneup$gene_name = idmap$orthologue(aneup$gene_name, dset="mmusculus_gene_ensembl",
+#                                   from="mgi_symbol", to="hgnc_symbol")
+
+both = list(stat1=stat1$rev48_stat1_over_wt, aneup=aneup) %>%
+    bind_rows(.id="dset") %>%
+    filter(padj < 0.1) %>%
+    transmute(dset = dset,
+              gene_name = gene_name,
+              stat = log2FoldChange / lfcSE) %>%
+    tidyr::pivot_wider(names_from="dset", values_from="stat") %>%
+    na.omit()
+
+ggplot(both, aes(x=stat1, y=aneup)) +
+    geom_point() +
+    ggrepel::geom_text_repel(aes(label=gene_name))
+
+sets_human = io$load(args$set_human) %>%
+    gset$filter(min=5, valid=stat1[[1]]$gene_name)
+sets_mouse = io$load(args$set_mouse) %>%
+    gset$filter(min=5, valid=aneup[[1]]$gene_name)
+
+pdf(args$plotfile)
+for (rname in names(res))
+    print(util$plot_gset(res[[rname]], sets) + ggtitle(rname))
+dev.off()
