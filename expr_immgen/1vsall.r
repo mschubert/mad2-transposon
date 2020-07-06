869c3affe791517abf3c9847ca880100694d9091
mschubert <mschu.dev@gmail.com>
2020-07-06 11:58:35 +0200

    add immgen vs tps 1vall

---
+++ expr_immgen/1vsall.r
@@ -0,0 +1,56 @@
+library(dplyr)
+library(SummarizedExperiment)
+io = import('io')
+
+de_1vsall = function(eset, cur) {
+    colData(eset)$is_cur = colData(eset)$Short == cur
+    design(eset) = ~ is_cur
+    res = DESeq2::DESeq(eset) %>%
+        DESeq2::results(name="is_curTRUE") %>%
+        as.data.frame() %>%
+        tibble::rownames_to_column("gene_name") %>%
+        as_tibble() %>%
+        arrange(padj)
+}
+
+annot = readxl::read_xlsx("../data/immgen/immgen_keypop.xlsx")
+immgen = readr::read_csv("../data/immgen/GSE109125_Gene_count_table.csv.gz")
+coldata = tibble(id=colnames(immgen)[-1]) %>%
+    mutate(Short = sub("#[0-9]+$", "", id)) %>%
+    inner_join(annot)
+expr = data.matrix(immgen[-1])
+rownames(expr) = immgen$Gene_Symbol
+expr = expr[,coldata$id]
+expr = expr[rowMeans(expr) >= 1,]
+# update gene symbols??
+expr = DESeq2::DESeqDataSetFromMatrix(expr, coldata, ~1)
+immgen_de = lapply(unique(coldata$Short), de_1vsall, eset=expr)
+names(immgen_de) = unique(coldata$Short)
+
+tps = io$load("../expr_diff/eset_Mad2PB.RData")$eset
+colData(tps)$Short = colnames(tps)
+tps_de = lapply(unique(colnames(tps)), de_1vsall, eset=tps)
+names(tps_de) = unique(colnames(tps))
+
+saveRDS(list(immgen_de=immgen_de, immgen_meta=annot,
+             tps_de=tps_de, tps_meta=colData(tps)), file="1vsall.rds")
+
+img = lapply(immgen_de, function(x) setNames(x$stat, x$gene_name)) %>%
+    narray::stack(along=2)
+img = img[rowMaxs(abs(img)) > 3 & !is.na(rownames(img)),]
+tps = lapply(tps_de, function(x) setNames(x$stat, x$gene_name)) %>%
+    narray::stack(along=2)
+tps = tps[rowMaxs(abs(tps)) > 3 & !is.na(rownames(tps)),]
+narray::intersect(img, tps, along=1) # loses a lot of genes
+
+cosine = function(x, y) sum(x * y) / (sqrt(sum(x^2)) * sqrt(sum(y^2)))
+res = narray::lambda(~ cosine(tps, img), along=c(tps=2, img=2))
+clust = reshape2::melt(res) %>%
+    plt$cluster(value ~ img + tps) %>%
+    as_tibble()
+pdf("1vsall.pdf", 5, 12)
+ggplot(clust, aes(x=img, y=tps)) +
+    geom_raster(aes(fill=value)) +
+    scale_fill_distiller(palette="Spectral") +
+    theme(axis.text.x = element_text(angle=45, hjust=1))
+dev.off()
