cbf1e713bf5fb8e1335f0e4c724c74fa0b48f137
mschubert <mschu.dev@gmail.com>
2021-02-04 11:20:15 +0100

    add enhancer assocs

---
+++ cis_analysis/ext_epi.r
@@ -0,0 +1,87 @@
+library(dplyr)
+b = import('base')
+io = import('io')
+sys = import('sys')
+plt = import('plot')
+
+#' Test different insertion statistics of a gene with an external variable
+#'
+#' This currently does not correct for when a cancer type is both more aneuploid
+#' and has more insertions in a given gene.
+#'
+#' @param dset  data.frame with fields: sample, epi, ext_var[, subset]
+#' @param epi  id to test
+#' @param ext_var  differential insertion rate with what, e.g. 'aneuploidy'
+#' @param is_type  filter dset with specific 'type' (default: all)
+#' @return  data.frame with tidy association results
+test_epi = function(dset, epi, ext_var, is_type=NA) {
+    `%>%` = magrittr::`%>%`
+    if (is.na(is_type))
+        is_type = unique(dset$type)
+    tset = dset %>%
+        dplyr::filter(type %in% is_type, id == epi) %>%
+        dplyr::mutate(ext = !! rlang::sym(ext_var))
+    broom::tidy(glm(reads ~ ext, family=poisson(), data=tset)) %>%
+        dplyr::mutate(size = sum(tset$reads, na.rm=TRUE)) %>%
+        dplyr::filter(term == "ext") %>%
+        dplyr::select(-term)
+}
+
+#' Volcano plot
+#'
+#' @param res  association result data.frame: external_gene_name, p.value, estimate
+#' @return  ggplot2 object
+plot_volcano = function(res) {
+    res %>%
+        filter(size >= 2) %>%
+        mutate(label = id) %>%
+        plt$p_effect("p.value", thresh=0.1) %>%
+        plt$volcano(p=0.1, label_top=30, repel=TRUE) +
+            ylab("non-adj. p-value")
+}
+
+sys$run({
+    args = sys$cmd$parse(
+        opt('m', 'meta', 'meta+aneuploidy', '../ploidy_compare/analysis_set.RData'),
+        opt('p', 'poisson', 'cis assocs RData', 'poisson_epi.RData'),
+        opt('o', 'outfile', 'external assocs RData', 'ext_epi.RData'),
+        opt('p', 'plotfile', 'pdf', 'ext_epi.pdf'))
+
+    meta = io$load(args$meta) %>%
+        mutate(aneuploidy = pmin(aneuploidy, 0.2))
+    dset = io$load(args$poisson)
+    genes = with(dset, intersect(samples$id, result$id))
+
+    aset = dset$samples %>%
+        select(-n_ins) %>%
+        filter(id %in% genes) %>%
+        mutate(reads=TRUE) %>%
+        tidyr::complete(sample, id, fill=list(reads=FALSE)) %>%
+        inner_join(dset$sample_rates %>% select(sample, n_ins, n_reads, rate)) %>%
+        inner_join(meta %>% select(sample, aneuploidy, type,
+                                   `T-cell`, Myeloid, Other))
+
+    fields = c("aneuploidy", "T-cell", "Myeloid", "Other")
+    result = expand.grid(id=genes, ext=fields,
+            type=unique(aset$type), stringsAsFactors=FALSE) %>%
+        filter(is.na(type) | ext == "aneuploidy") %>%
+        mutate(res = clustermq::Q(test_epi, const=list(dset=aset),
+            epi=id, ext_var=ext, is_type=type,
+            job_size=50, n_jobs=20, memory=1024)) %>%
+        tidyr::unnest() %>%
+        mutate(cohens_d = statistic / sqrt(size))
+
+    results = result %>%
+        mutate(subset = ifelse(is.na(type), ext, paste0(type, ":", ext))) %>%
+        select(-ext, -type) %>%
+        split(.$subset)
+
+    pdf(args$plotfile)
+    for (rn in names(results)) {
+        message(rn)
+        print(plot_volcano(results[[rn]]) + ggtitle(rn))
+    }
+    dev.off()
+
+    save(results, file=args$outfile)
+})
---
+++ cis_analysis/poisson_epi.r
@@ -0,0 +1,83 @@
+library(dplyr)
+library(plyranges)
+io = import('io')
+seq = import('seq')
+sys = import('sys')
+plt = import('plot')
+
+args = sys$cmd$parse(
+    opt('i', 'infile', 'transposon insert RData', 'analysis_set.RData'),
+    opt('u', 'upstream', 'bp to include before gene', '5000'),
+    opt('d', 'downstream', 'bp to include after gene', '5000'),
+    opt('o', 'outfile', 'insertion statistics RData', 'poisson_epi.RData'),
+    opt('p', 'plotfile', 'volcano pdf', 'poisson_epi.pdf'))
+
+ins = io$load(args$infile) %>%
+    filter(chr %in% c(1:19, 'X'), !is_local) %>%
+    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns=TRUE,
+        start.field="position", end.field="position")
+
+genome = seq$genome("GRCm38", chrs=c(1:19, 'X'))
+
+# time out
+## biomart query ensembl mouse regulation
+#ensembl = biomaRt::useEnsembl("ENSEMBL_MART_FUNCGEN", dataset="mmusculus_regulatory_feature")
+#attrs = c("chromosome_name", "chromosome_start", "chromosome_end", "feature_type_name",
+#          "regulatory_stable_id", "epigenome_name", "activity")
+#res = biomaRt::getBM(attributes=attrs, filters=c("epigenome_name", "activity"),
+#                     values=list(c("spleen adult","thymus adult"), "ACTIVE"), mart=ensembl)
+res = readr::read_tsv("poisson_epi.tsv") %>%
+    filter(`Feature type` == "Enhancer") %>%
+    dplyr::rename(chr = `Chromosome/scaffold name`,
+                  start = `Start (bp)`,
+                  end = `End (bp)`,
+                  id = `Regulatory stable ID`) %>%
+    select(chr, start, end, id) %>%
+    distinct() %>%
+    makeGRangesFromDataFrame(keep.extra.columns=TRUE) %>%
+    filter(seqnames(.) %in% seqnames(ins)) %>%
+    anchor_center %>% stretch(10000) %>%
+    mutate(TTAAs = seq$count_pattern("TTAA", genome, ., rc=TRUE)) %>%
+    filter(TTAAs > 0) # if no upstream, Mir5136 has 0 TTAAs, 1 insert
+
+ins_sites_genome = seq$count_pattern("TTAA", genome, rc=TRUE)
+n_smp = length(unique(ins$sample))
+sample_rates = as.data.frame(ins) %>%
+    group_by(sample) %>%
+    summarize(n_ins = dplyr::n(),
+              n_reads = sum(reads)) %>%
+    mutate(rate = n_ins / ins_sites_genome)
+ins_rate_genome = mean(sample_rates$rate)
+
+samples = seq$intersect(ins, res) %>%
+    select(sample, id, reads) %>%
+    group_by(sample, id) %>%
+    summarize(n_ins = dplyr::n(),
+              reads = sum(reads)) %>%
+    ungroup()
+
+ptest = function(n_ins, len, rate) broom::tidy(poisson.test(n_ins, len*n_smp, rate))
+result = as.data.frame(GenomicRanges::mcols(res)) %>%
+    inner_join(samples %>% select(sample, id)) %>%
+    group_by(id, TTAAs) %>%
+        summarize(n_smp = n_distinct(sample)) %>%
+    ungroup() %>%
+    mutate(result = purrr::map2(n_smp, TTAAs, ptest, rate=ins_rate_genome)) %>%
+    tidyr::unnest() %>%
+    select(-(parameter:alternative)) %>%
+    mutate(estimate = log2(estimate / ins_rate_genome),
+           adj.p = p.adjust(p.value, method="fdr")) %>%
+    arrange(adj.p, p.value)
+
+p = result %>%
+    mutate(size = n_smp,
+           label = id) %>%
+    plt$p_effect() %>%
+    plt$volcano(label_top=30, repel=TRUE, ceil=1e-30) +
+        xlab("log2 fold change poisson rate")
+
+pdf(args$plotfile)
+print(p)
+dev.off()
+
+save(samples, genes, sample_rates, result, file=args$outfile)
