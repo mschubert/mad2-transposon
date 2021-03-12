9775d966cea7fbeb96719f01b96f677677b7b1c4
mschubertv <mschu.dev@gmail.com>
2021-03-12 14:14:17 +0000

    mouse IFN treatment + summary stats

---
+++ data/rnaseq_ifn/rnaseq.r
@@ -0,0 +1,62 @@
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+sys = import('sys')
+
+plot_pca = function(eset) {
+    vst = DESeq2::varianceStabilizingTransformation(eset)
+    pcadata = DESeq2::plotPCA(vst, intgroup=c("sample_id", "mouse", "cell_type", "treatment"), returnData=TRUE) %>%
+        mutate(sample_id = sub(".*_([0-9]+)$", "\\1", sample_id))
+    pcavar = round(100 * attr(pcadata, "percentVar"))
+
+    ggplot(pcadata, aes(PC1, PC2)) +
+        geom_point(aes(shape=treatment, color=cell_type), size=3, alpha=0.8) +
+        ggrepel::geom_text_repel(aes(label=sample_id), max.overlaps=Inf) +
+        xlab(paste0("PC1: ", pcavar[1], "% variance")) +
+        ylab(paste0("PC2: ", pcavar[2], "% variance"))
+}
+
+file2deseq = function(fname, samples) {
+    rtab = readr::read_tsv(fname)
+    reads = data.matrix(rtab[-1])
+    rownames(reads) = rtab$Gene
+
+    eset = DESeq2::DESeqDataSetFromMatrix(reads, samples, ~1) %>%
+        DESeq2::estimateSizeFactors()
+}
+
+plot_overview = function(eset, title="") {
+    rc = tibble(sample_id = factor(sub(".*_([0-9]+)$", "\\1", colnames(eset))),
+                read_count = colSums(DESeq2::counts(eset, normalized=FALSE)))
+    levels(rc$sample_id) = gtools::mixedsort(levels(rc$sample_id))
+    prc = ggplot(rc, aes(x=sample_id, y=read_count)) +
+        geom_col() +
+        ggtitle(title)
+    ppca = plot_pca(eset)
+
+    prc | ppca
+}
+
+sys$run({
+    args = sys$cmd$parse(
+        opt('s', 'samples', 'tsv', 'samples.tsv'),
+        opt('o', 'outfile', 'rds', 'rnaseq.rds'),
+        opt('p', 'plotfile', 'pdf', 'rnaseq.pdf')
+    )
+
+    tab = readxl::read_excel("Total_gene_counts.xls")
+    samples = readr::read_tsv(args$samples)
+
+    fnames = c(#"SCRB_CH210203_UMIcounts_only_plus.txt.gz",
+               "SCRB_CH210203_UMIcounts_only_minus.txt.gz",
+               "SCRB_CH210203_UMIcounts_both_strands.txt.gz")
+
+    esets = lapply(fnames, file2deseq, samples=samples)
+    plots = mapply(plot_overview, eset=esets, title=fnames, SIMPLIFY=FALSE)
+
+    pdf(args$plotfile, 12, 7)
+    print(ggplot() + annotation_custom(gridExtra::tableGrob(samples)))
+    for (p in plots)
+        print(p)
+    dev.off()
+})
