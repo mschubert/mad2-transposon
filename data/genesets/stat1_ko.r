3760667976b6713b1da5c3c72efeb0223ddde1a7
mschubertv <mschu.dev@gmail.com>
2021-03-15 20:46:59 +0000

    add mouse gsva

--- data/genesets/stat1_ko.r
+++ data/genesets/stat1_ko.r
@@ -1,14 +1,17 @@
 library(dplyr)
 sys = import('sys')
-idmap = import('process/idmap')
+gset = import('genesets')
 
 args = sys$cmd$parse(
     opt('d', 'diff_expr', 'rds', '../../expr_stat1/diff_expr.rds'),
-    opt('h', 'human', 'save to RData', 'human/stat1_ko.rds')#,
-#    opt('m', 'mouse', 'save to RData', 'mouse/stat1_ko.rds')
+    opt('h', 'human', 'save to rds', 'human/stat1_ko.rds'),
+    opt('m', 'mouse', 'save to rds', 'mouse/stat1_ko.rds')
 )
 
 sets = readRDS(args$diff_expr) %>%
     lapply(. %>% filter(log2FoldChange > 0) %>% head(100) %>% pull(gene_name))
 
+mouse = gset$hu2mouse(sets)
+
 saveRDS(sets, file=args$human)
+saveRDS(mouse, file=args$mouse)
