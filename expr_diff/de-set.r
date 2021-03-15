e5c6ef335eff4d9214b08590f116a732a7387e2e
mschubertv <mschu.dev@gmail.com>
2021-03-15 15:14:07 +0000

    workflow for sets, detail

---
+++ expr_diff/de-set.r
@@ -0,0 +1,28 @@
+library(dplyr)
+library(ggplot2)
+sys = import('sys')
+gset = import('genesets')
+util = import('./util')
+
+args = sys$cmd$parse(
+    opt('c', 'config', 'yaml', '../config.yaml'),
+    opt('e', 'eset', 'gene expression rds', 'eset_Mad2PB.rds'),
+    opt('d', 'diff_expr', 'rds', 'de_Mad2PB.rds'),
+    opt('s', 'setfile', 'rds', '../data/genesets/mouse/MSigDB_Hallmark_2020.rds'),
+    opt('o', 'outfile', 'results rds', 'de_Mad2PB/MSigDB_Hallmark_2020.rds'),
+    opt('p', 'plotfile', 'pdf', 'de_Mad2PB/MSigDB_Hallmark_2020.pdf')
+)
+
+hl = yaml::read_yaml(args$config)$highlight_de
+eset = readRDS(args$eset)$eset
+res = readRDS("de_Mad2PB.rds")
+
+sets = readRDS(args$setfile) %>%
+    gset$filter(min=5, valid=rownames(eset))
+
+pdf(args$plotfile)
+for (rname in names(res))
+    print(util$plot_gset(res[[rname]], sets) + ggtitle(rname))
+dev.off()
+
+saveRDS(res, file=args$outfile)
--- expr_diff/de_Mad2PB.r
+++ expr_diff/de_Mad2PB.r
@@ -1,16 +1,13 @@
 library(dplyr)
 library(ggplot2)
-library(DESeq2)
 sys = import('sys')
-plt = import('plot')
-gset = import('data/genesets')
 util = import('./util')
 
 #' Fit tissue of origin (vs. mean) and pan-aneuploidy (tissue and type-corrected)
 de_type = function(eset, ats=c("Tcell", "Myeloid", "Other")) {
     res = util$do_wald(eset, ~ tissue + type + aneuploidy, ex="tissue|aneuploidy")
     cancer_type = function(term) {
-        design(eset) = formula(paste("~ tissue + ", term))
+        DESeq2::design(eset) = formula(paste("~ tissue + ", term))
         DESeq2::estimateDispersions(eset) %>%
             DESeq2::nbinomWaldTest(maxit=1000) %>%
             util$extract_coef(term)
@@ -23,26 +20,14 @@ de_per_type = function(eset, ats=c("Tcell", "Myeloid", "Other")) {
     aneup_tissue = function(type) {
         eset = eset[,eset[[type]] == 1]
         if (length(unique(eset$tissue)) == 1)
-            design(eset) = formula(paste("~ aneuploidy"))
+            DESeq2::design(eset) = formula(paste("~ aneuploidy"))
         else
-            design(eset) = formula(paste("~ aneuploidy + tissue"))
+            DESeq2::design(eset) = formula(paste("~ aneuploidy + tissue"))
         DESeq2::estimateDispersions(eset) %>%
             DESeq2::nbinomWaldTest(maxit=1000) %>%
             util$extract_coef("aneuploidy")
     }
     setNames(lapply(ats, aneup_tissue), paste0(ats, ":aneuploidy"))
-}
-
-plot = function(res, sets=list(), hl=c()) {
-    for (rname in names(res)) {
-        message(rname)
-        print(util$plot_volcano(res[[rname]], hl) + ggtitle(rname))
-        for (sname in names(sets)) {
-            title = paste(rname, sname)
-            message(title)
-            print(util$plot_gset(res[[rname]], sets[[sname]]) + ggtitle(title))
-        }
-    }
 }
 
 sys$run({
@@ -51,25 +36,19 @@ sys$run({
         opt('c', 'config', 'yaml', '../config.yaml'),
         opt('n', 'network', 'rds', '../data/networks/E-GEOD-13159.rds'), # ignored
         opt('o', 'outfile', 'results rds', 'de_Mad2PB.rds'),
-        opt('p', 'plotfile', 'pdf', 'de_Mad2PB.pdf'),
-        arg('sets', 'gene set .rds', arity='*',
-            list.files("../data/genesets/mouse", "\\.rds", full.names=TRUE))
+        opt('p', 'plotfile', 'pdf', 'de_Mad2PB.pdf')
     )
 
+    hl = yaml::read_yaml(args$config)$highlight_de
     eset = readRDS(args$eset)$eset
+
     de_type = de_type(eset)
     de_per_type = de_per_type(eset)
-
-    sets = lapply(args$sets, readRDS) %>%
-        setNames(tools::file_path_sans_ext(basename(args$sets))) %>%
-        lapply(function(x) gset$filter(x, min=5, valid=rownames(eset)))
-
-    hl = yaml::read_yaml(args$config)$highlight_de
-
     res = c(de_type, de_per_type)
 
     pdf(args$plotfile)
-    plot(res, sets, hl)
+    for (rname in names(res))
+        print(util$plot_volcano(res[[rname]], hl) + ggtitle(rname))
     dev.off()
 
     saveRDS(res, file=args$outfile)
