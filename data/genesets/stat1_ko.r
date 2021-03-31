2cf6f07c7f32d8061b22b561bc865102dc8bbdaf
mschubertv <mschu.dev@gmail.com>
2021-03-31 15:31:22 +0000

    add hm+dorothea

--- data/genesets/stat1_ko.r
+++ data/genesets/stat1_ko.r
@@ -8,8 +8,17 @@ args = sys$cmd$parse(
     opt('m', 'mouse', 'save to rds', 'mouse/stat1_ko.rds')
 )
 
+# BT549 STAT1 KO differential expression
 sets = readRDS(args$diff_expr) %>%
     lapply(. %>% filter(log2FoldChange > 0) %>% head(100) %>% pull(gene_name))
+
+# DoRothEA STAT1 binding + inflammatory MSigDB Hallmarks
+gets = gset$get_human(c("MSigDB_Hallmark_2020", "DoRothEA"), conf="a")
+msets = c("Interferon Gamma Response", "Interferon Alpha Response",
+          "Inflammatory Response", "TNF-alpha Signaling via NF-kB",
+          "IL-6/JAK/STAT3 Signaling")
+msig = gets$MSigDB_Hallmark_2020[msets] %>% unlist() %>% unique()
+sets$`Stat1+IFN` = intersect(msig, gets$DoRothEA$`STAT1 (a)`)
 
 mouse = gset$hu2mouse(sets)
 
