8a1f6a0ca61fc13d9b96cbec5c7b0bed61b5877a
mschubert <mschu.dev@gmail.com>
2021-04-02 13:43:56 +0200

    landscape format

--- report/Fig2-CIS.r
+++ report/Fig2-CIS.r
@@ -19,7 +19,7 @@ insertion_matrix = function(cis, rna_ins, cis_aneup, aneup, net_genes) {
     cis_result = cis$result %>%
         filter(! external_gene_name %in% c("Sfi1", "Drg1", "Eif4enif1", "Wrap53"), # should blacklist those when mapping
                external_gene_name %in% net_genes) %>%
-        filter(adj.p < 0.01 | (adj.p < 0.05 & external_gene_name %in% cis_aneup$external_gene_name))
+        filter(adj.p < 0.015)
     cis_samples = cis$samples %>%
         filter(external_gene_name %in% cis_result$external_gene_name) %>%
         tidyr::complete(sample, external_gene_name, fill=list(n_ins=0, reads=0)) %>%
@@ -99,13 +99,13 @@ insertion_matrix = function(cis, rna_ins, cis_aneup, aneup, net_genes) {
 
     p11 + plot_spacer() + (p12 + plot_layout(tag_level="new")) + plot_spacer() +
         p1 + (p13 + plot_layout(tag_level="new")) +
-        plot_layout(widths=c(6,1), heights=c(1,0.1,5), guides="collect") &
-        theme(plot.margin=margin(0.25, 0, 0.25, 2, "mm"))
+        plot_layout(widths=c(6,1), heights=c(1,0.12,5), guides="collect") &
+        theme(plot.margin=margin(0.25, -15, 0.25, 2, "mm"))
 }
 
 subtype_assocs = function(ext, net_genes) {
     lvl = setNames(c("Myeloid", "T-ALL", "B-like", "Aneuploidy"),
-                   c("Myeloid", "T-cell", "Other", "aneuploidy"))
+                   c("Myeloid", "T-cell", "B-like", "aneuploidy"))
     types = bind_rows(ext) %>%
         filter(subset %in% names(lvl),
                external_gene_name %in% net_genes) %>%
@@ -115,19 +115,19 @@ subtype_assocs = function(ext, net_genes) {
     types$subset = unname(lvl[types$subset])
     types$subset = factor(types$subset, levels=unname(lvl))
 
-    p = ggplot(types, aes(x=forcats::fct_reorder(external_gene_name, statistic), y=statistic)) +
+    ggplot(types, aes(x=forcats::fct_reorder(external_gene_name, statistic), y=statistic)) +
         geom_hline(yintercept=0, color="grey", linetype="dashed") +
         geom_bar(aes(fill=subset), stat="identity") +
         geom_text(aes(label=external_gene_name, y=statistic/2)) +
         coord_flip() +
-        facet_wrap(~ subset, scales="free_y", ncol=1) +
+        facet_wrap(~ subset, scales="free_y") +
         theme(axis.title.y = element_blank(),
               axis.text.y = element_blank(),
               axis.line = element_blank(),
-              axis.ticks.y = element_blank()) +
+              axis.ticks.y = element_blank(),
+              plot.margin = margin(-5,3,-5,3,"mm")) +
         guides(fill = FALSE) +
         labs(y = "Wald statistic")
-    wrap_plots(wrap_elements(p))
 }
 
 bionet_combine = function(bionet) {
@@ -145,16 +145,16 @@ bionet_combine = function(bionet) {
         arrange(-hub) %>%
         left_join(aneup_centrality, copy=TRUE) %>%
         mutate(aneup_hub = ifelse(is.na(aneup_hub), 0, aneup_hub))
-    p = ggraph(cnet) +
+    ggraph(cnet) +
         geom_edge_link(alpha=0.05, width=3) +
         geom_node_point(aes(size=hub, fill=aneup_hub), color="black", shape=21) +
         scale_fill_distiller(palette="RdPu", direction=1) +
         geom_node_label(aes(label=external_gene_name, size=hub), repel=TRUE,
                         label.size=NA, segment.alpha=0.3, fill="#ffffff00") +
-        scale_size(range = c(2.5,12)) +
+        scale_size(range = c(2.5,10)) +
         guides(fill = guide_legend(title="Aneuploidy\ncentrality", override.aes=list(size=5)),
-               size = guide_legend(title="CIS centrality"))
-    wrap_plots(p)
+               size = guide_legend(title="CIS centrality")) +
+        theme(plot.margin = margin(0,0,0,0,"mm"))
 }
 
 sc_wgs = function() {
@@ -175,9 +175,7 @@ sys$run({
     )
 
     aneup = readRDS(args$aset)$meta %>%
-        select(genotype, sample, type, aneuploidy) %>%
-        mutate(type = factor(type))
-    levels(aneup$type)[levels(aneup$type) == "Other"] = "B-like"
+        select(genotype, sample, type, aneuploidy)
     ext = readRDS(args$ext)
     bionet = readRDS(args$bionet)
     net_genes = bionet$cis_net %N>% pull(external_gene_name)
@@ -190,18 +188,18 @@ sys$run({
     schema = grid::rasterGrob(magick::image_read("external/CISschema.svg"))
     splot = ggplot() + annotation_custom(schema) +
         theme(axis.line=element_blank())
-    sc_wgs = sc_wgs()
-    ins_mat = insertion_matrix(cis, rna_ins, ext$aneuploidy, aneup, net_genes)
-    stype = subtype_assocs(ext, net_genes)
-    bnet = bionet_combine(bionet)
+    sc_wgs = sc_wgs() + theme(panel.ontop = FALSE)
+    ins_mat = wrap_plots(wrap_elements(insertion_matrix(cis, rna_ins, ext$aneuploidy, aneup, net_genes)))
+    stype = wrap_plots(wrap_elements(subtype_assocs(ext, net_genes)))
+    bnet = wrap_plots(wrap_elements(bionet_combine(bionet)))
 
-    asm = ((splot + sc_wgs + plot_layout(widths=c(2,3))) /
-        ins_mat /
-        (stype + bnet + plot_layout(widths=c(2,7)))) +
-            plot_layout(heights=c(1.2,3,2)) + plot_annotation(tag_levels='a') &
-        theme(plot.tag = element_text(size=18, face="bold"))
+    top = (splot | sc_wgs | plt$text("CIN<>aneup goes here") | stype) + plot_layout(widths=c(4,5.2,3,3))
+    bottom =  wrap_plots(ins_mat) + bnet + plot_layout(widths=c(1.8,1))
 
-    pdf("Fig2-CIS.pdf", 14, 19)
+    asm = (top / bottom) + plot_layout(heights=c(1,2.5)) +
+        plot_annotation(tag_levels='a') & theme(plot.tag = element_text(size=18, face="bold"))
+
+    pdf("Fig2-CIS.pdf", 20, 12)
     print(asm)
     dev.off()
 })
