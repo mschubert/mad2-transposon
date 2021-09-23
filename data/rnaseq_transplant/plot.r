10d9bb7c28e4cb49bda3acc003097a2b6aa32169
mschubertv <mschu.dev@gmail.com>
2021-09-23 18:44:53 +0000

    more space for GO

--- data/rnaseq_transplant/plot.r
+++ data/rnaseq_transplant/plot.r
@@ -7,7 +7,8 @@ plt = import('plot')
 plot_one = function(title, comp) {
     plots = mapply(function(c, n) plt$volcano(c, label_top=30, repel=TRUE) + ggtitle(n),
         c=comp, n=names(comp), SIMPLIFY=FALSE)
-    plt$text(title) / wrap_plots(plots, nrow=1) + plot_layout(heights=c(1,20))
+    plt$text(title, size=6) / wrap_plots(plots, design="ABD\nECC") +
+        plot_layout(heights=c(1,40))
 }
 
 args = sys$cmd$parse(
@@ -17,7 +18,7 @@ args = sys$cmd$parse(
 
 res = readRDS(args$diff_expr)
 
-pdf(24, 8, file=args$plotfile)
+pdf(16, 14, file=args$plotfile)
 for (i in seq_along(res))
     print(plot_one(names(res)[i], res[[i]]))
 dev.off()
