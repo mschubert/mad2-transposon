03d576169bae12426dc65490119c60eede73b2c0
mschubertv <mschu.dev@gmail.com>
2021-09-24 15:08:37 +0000

    ins volcanos + networks

--- report/FigS2-CIS.r
+++ report/FigS2-CIS.r
@@ -1,16 +1,36 @@
 library(dplyr)
 library(ggplot2)
+library(ggraph)
 library(patchwork)
 theme_set(cowplot::theme_cowplot())
 sys = import('sys')
 plt = import('plot')
+bnet = import('../cis_analysis/bionet')
 
-sc_wgs = function() {
-    smps = c("401t", "419t", "413s")
-    scs = file.path("../data/wgs", paste0(smps, ".rds")) %>%
-        lapply(readRDS) %>%
-        setNames(smps)
-    plt$genome$heatmap_aneuHMM(scs) + guides(fill = guide_legend(title="Copy number"))
+cis_row = function(bn, assocs) {
+    p1 = plt$volcano(assocs, size="n_smp", label_top=50, max.overlaps=50, x_label_bias=0.5)
+    p2 = ggraph(bn$cis_net) +
+        geom_node_point(aes(size=n_smp)) +
+        geom_node_text(aes(label=name), repel=TRUE) +
+        geom_edge_link(alpha=0.2) +
+        theme_void()
+    p1 + p2 + plot_layout(widths=c(1,2))
+}
+
+aneup_row = function(bn, ext) {
+    p1 = plt$volcano(ext, label_top=30)
+    net_with_stats = bn$ext_nets$aneuploidy %>%
+        left_join(ext %>% select(name=external_gene_name, p.value, statistic))
+    p2 = ggraph(net_with_stats) +
+        geom_node_point(aes(size=n_smp, fill=statistic, stroke=p.value<0.05,
+                                     color=p.value<0.05), shape=21) +
+        geom_node_text(aes(label=name), repel=TRUE) +
+        geom_edge_link(alpha=0.2) +
+        theme_void() +
+        scale_fill_gradient2(low="red", mid="white", high="blue", midpoint=0) +
+        scale_color_manual(name="signif", labels=c("n.s.", "p<0.05"),
+                           values=c("white", "black"))
+    p1 + p2 + plot_layout(widths=c(1,2))
 }
 
 sys$run({
@@ -23,20 +43,18 @@ sys$run({
         opt('p', 'plotfile', 'pdf', 'FigS2-CIS.pdf')
     )
 
-#    aneup = readRDS(args$aset)$meta %>%
-#        select(genotype, sample, type, aneuploidy) %>%
-#        mutate(type = factor(type))
-#    levels(aneup$type)[levels(aneup$type) == "Other"] = "B-like"
-#    ext = readRDS(args$ext)
+    bn = readRDS("../cis_analysis/bionet_omnipath.rds")
+    cis_nodes = igraph::V(bn$cis_net)$name
+#    an_nodes = igraph::V(bn$ext_nets$aneuploidy)$name
 
-    # insertion processing
-#    rna_ins = readr::read_tsv(args$rna_ins, header=TRUE)
-#    cis = readRDS(args$poisson)
+    assocs = readRDS("../cis_analysis/poisson.rds")$result %>%
+        filter(! external_gene_name %in% c("Sfi1", "Drg1")) %>% # excl genome region
+        mutate(circle = external_gene_name %in% cis_nodes)
+    ext = readRDS("../cis_analysis/ext_gene.rds")$aneuploidy %>%
+        mutate(circle = external_gene_name %in% cis_nodes)
 
-    # create plot objects
-    sc_wgs = sc_wgs()
-
-    asm = sc_wgs + plot_annotation(tag_levels='a') &
+    asm = (cis_row(bn, assocs) / aneup_row(bn, ext)) +
+        plot_annotation(tag_levels='a') +
         theme(plot.tag = element_text(size=18, face="bold"))
 
     pdf(args$plotfile, 10, 6)
