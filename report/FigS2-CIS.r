32c5982fcd5ce025b7cfb7c5fbf2281ba7c08c4a
mschubertv <mschu.dev@gmail.com>
2021-09-25 19:26:00 +0000

    add insertion plots (not integrated yet)

--- report/FigS2-CIS.r
+++ report/FigS2-CIS.r
@@ -6,7 +6,62 @@ library(patchwork)
 theme_set(cowplot::theme_cowplot())
 sys = import('sys')
 plt = import('plot')
+seq = import('seq')
 bnet = import('../cis_analysis/bionet')
+tx = seq$coords$transcript(dset="mmusculus_gene_ensembl", assembly="GRCm38")
+
+ins_distr = function(gene, rna_ins, dna_ins) {
+    txg = tx %>% filter(external_gene_name == gene)
+    rng = txg %>% group_by(ensembl_transcript_id) %>%
+        summarize(start=min(transcript_start), end=max(transcript_end))
+
+    exon = ensembldb::exonsBy(EnsDb.Mmusculus.v79::EnsDb.Mmusculus.v79,
+                              filter=AnnotationFilter::GeneIdFilter(txg$ensembl_gene_id[1])) %>%
+        as.data.frame() %>% as_tibble() %>%
+        select(ensembl_transcript_id=group_name, exon_id, start, end) %>%
+        mutate(type="transcript")
+    strn = c("1"="+","-1"="-")[as.character(txg$strand[1])]
+
+    ri = rna_ins %>% filter(gene_name == gene, support >= 2) %>%
+        mutate(strand=c("1"="+","-1"="-")[as.character(strand)])
+    di = dna_ins %>% filter(chr == txg$chromosome_name[1],
+                            position >= min(rng$start),
+                            position <= max(rng$end)) %>%
+        group_by(sample) %>%
+            slice_max("reads", n=1) %>%
+        ungroup()
+    both = bind_rows(list(RNA=ri, DNA=di), .id="type") %>%
+        mutate(type = factor(type, levels=c("DNA", "RNA")),
+               sense = factor(strand == strn, levels=c("TRUE", "FALSE")))
+    levels(both$sense) = c("sense", "antisense")
+
+    p1 = ggplot(both, aes(y=1, x=position)) +
+        ggbeeswarm::geom_quasirandom(aes(shape=factor(strand), color=type, alpha=sense), size=7, groupOnX=FALSE) +
+        geom_text(aes(label=sample), size=3, position=ggbeeswarm::position_quasirandom(groupOnX=FALSE)) +
+        scale_shape_manual(values = c("+"="\u2b95", "-"="\u2b05"), drop=FALSE, guide="none") +
+        scale_alpha_manual(values = c("sense"=0.9, "antisense"=0.4), drop=FALSE, name="Orientation") +
+        scale_color_discrete(drop=FALSE, name="Evidence") +
+        ggtitle(gene) +
+        theme(axis.line.y = element_blank(),
+              axis.title = element_blank(),
+              axis.ticks.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.text.x = element_text(size=10)) +
+        labs(subtitle = sprintf("chr%s:%.1f-%.1f Mb (%s)", txg$chromosome_name[1],
+                                min(rng$start)/1e6, max(rng$end)/1e6, strn))
+
+    p2 = ggplot(txg, aes(y=ensembl_transcript_id, yend=ensembl_transcript_id)) +
+        geom_segment(data=rng, aes(x=start, xend=end), size=1, color="#34343456") +
+        geom_segment(data=exon, aes(x=start, xend=end), size=5, color="#343434d0") +
+        theme_void()
+
+    p1 / p2 + plot_layout(heights=c(1,1))
+}
+
+all_ins = function(rna_ins, dna_ins) {
+    ps = lapply(c("Ets1", "Erg", "Trp53", "Stat1"), ins_distr, rna_ins=rna_ins, dna_ins=dna_ins)
+    wrap_plots(ps) + plot_layout(guides="collect") & theme(plot.margin=unit(c(5,5,5,5),"mm"))
+}
 
 cis_row = function(bn, assocs, common) {
     p1 = plt$volcano(assocs, size="n_smp", label_top=30, max.overlaps=50, x_label_bias=0.5) +
@@ -68,13 +123,15 @@ aneup_row = function(bn, ext, common) {
 
 sys$run({
     args = sys$cmd$parse(
-#        opt('a', 'aset', 'rds', '../ploidy_compare/analysis_set.rds'),
-        opt('p', 'poisson', 'rds', '../cis_analysis/poisson.rds'),
         opt('e', 'ext', 'rds', '../cis_analysis/ext_gene.rds'),
         opt('b', 'bionet', 'rds', '../cis_analysis/bionet_omnipath.rds'),
-#        opt('r', 'rna_ins', 'txt', '../data/rnaseq_imfusion/insertions.txt'),
+        opt('d', 'dna_ins', 'rds', '../cis_analysis/analysis_set.rds'),
+        opt('r', 'rna_ins', 'txt', '../data/rnaseq_imfusion/insertions.txt'),
         opt('p', 'plotfile', 'pdf', 'FigS2-CIS.pdf')
     )
+
+    rna_ins = readr::read_tsv(args$rna_ins)
+    dna_ins = readRDS(args$dna_ins)
 
     bn = readRDS(args$bionet)
     bn$cis_net = bn$cis_net %>% mutate(hub = centrality_hub())
@@ -90,6 +147,8 @@ sys$run({
     asm = (cis_row(bn, assocs, common) / aneup_row(bn, ext, common)) +
         plot_annotation(tag_levels='a') &
         theme(plot.tag = element_text(size=18, face="bold"))
+
+#    all_ins(rna_ins, dna_ins)
 
     pdf(args$plotfile, 18, 14)
     print(asm)
