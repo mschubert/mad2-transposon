3b8660ce64e297482950453cb1e92499ef8b50b9
mschubert <mschu.dev@gmail.com>
2022-02-22 15:05:00 +0100

    add tps, gdsc gex

--- misc/stat1-int-muts.r
+++ misc/stat1-int-muts.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidygraph)
 sys = import('sys')
+idmap = import('process/idmap')
 tcga = import('data/tcga')
 
 stat1int_mut = function() {
@@ -31,12 +32,40 @@ stat1int_mut = function() {
                       MBNL1 = length(intersect(gene, "MBNL1")),
                       TP53 = length(intersect(gene, "TP53")),
                       EGFR = length(intersect(gene, "EGFR")),
+                      TTN = length(intersect(gene, "TTN")),
                       stat1 = length(intersect(gene, "STAT1")),
                       ints = length(intersect(gene, ints)),
                       tot = length(intersect(gene, tot))) %>%
         ungroup() %>%
         mutate(frac = ints/tot) %>%
         arrange(-frac)
+
+    genes = c("Stat1", "Pias1", "Nfkb1", "Ptpn2", "Heg1", "Scaf11", "Ep300", "Trappc9", "Glis3", "Mbnl1", "Trp53", "Myc")
+    gex = readRDS("../data/rnaseq/assemble.rds")$expr
+    rownames(gex) = idmap$gene(rownames(gex), to="external_gene_name")
+    gex = gex[rownames(gex) %in% genes,] %>%
+       reshape2::melt() %>%
+       as_tibble() %>%
+       dplyr::rename(gene=Var1, sample=Var2, expr=value) %>%
+       inner_join(meta)
+    ggplot(gex, aes(x=gene, group=sample, y=expr)) +
+        ggbeeswarm::geom_quasirandom(aes(color=type))
+    gdsc = import('data/gdsc')
+    ge = gdsc$basal_expression()
+    ge = ge[rownames(ge) %in% toupper(genes),] %>%
+       reshape2::melt() %>%
+       as_tibble() %>%
+       dplyr::rename(gene=Var1, sample=Var2, expr=value) %>%
+       mutate(cline = gdsc$cosmic$id2name(sample),
+              tissue = gdsc$cosmic$id2tissue(sample),
+              is_BRCA = tissue == "BRCA",
+              label = ifelse(cline %in% c("BT-549", "MCF7", "MDA-MB-231", "A549"), cline, NA))
+    ggplot(ge, aes(x=gene, y=expr)) +
+        ggbeeswarm::geom_quasirandom(aes(alpha=is_BRCA, color=label, size=!is.na(label))) +
+        scale_alpha_manual(values=c("TRUE"=0.8, "FALSE"=0.02)) +
+        scale_color_brewer(palette="Set1", na.value="black") +
+        scale_size_manual(values=c("TRUE"=3, "FALSE"=1)) #+
+#        ggrepel::geom_label_repel(aes(label=label), size=2)
 
     xx = tcga$aneuploidy() %>%
         select(Sample, aneup=aneup_log2seg) %>%
@@ -52,7 +81,7 @@ stat1int_mut = function() {
         ggbeeswarm::geom_quasirandom(alpha=0.5, size=5) +
         scale_color_manual(values=setNames(c("#cc9933", "#5500aa"), c("(0,0.2]", "(0.2,Inf]")), name="Aneuploidy")
 
-    ggplot(xx, aes(x=LPP, y=tot, color=aneup_class, fill=aneup_class)) +
+    ggplot(xx, aes(x=TRAPPC9, y=tot, color=aneup_class, fill=aneup_class)) +
         geom_point(alpha=0.2) +
         geom_smooth(method="lm") +#, formula=y~s(x, k=10)) +
         scale_x_continuous(trans="log1p", breaks=c(0,1)) +
@@ -70,17 +99,20 @@ stat1int_mut = function() {
         arrange(-n_eup)
 
     zz = inner_join(mouse_ints %>% mutate(gene = toupper(external_gene_name)), yy) %>%
-        filter(size >= 3, p.value < 0.1) %>%
+        filter(size >= 3, p.value < 0.1, n_eup+1 >= n_aneup) %>%
         arrange(p.value)
 
 }
 
 sys$run({
     args = sys$cmd$parse(
+        opt('a', 'aset', 'rds', '../ploidy_compare/analysis_set.rds'),
         opt('e', 'ext', 'rds', '../cis_analysis/ext_gene.rds'),
         opt('b', 'bionet', 'rds', '../cis_analysis/bionet_omnipath.rds'),
         opt('p', 'plotfile', 'pdf', 'stat1-int-muts.pdf')
     )
+
+    meta = readRDS(args$aset)$meta
 
     # load mouse ins data from supp fig
     bn = readRDS(args$bionet)
