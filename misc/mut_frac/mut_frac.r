fdb158026f0174b4a314778504b40f2c8155ab0f
mschubertv <mschu.dev@gmail.com>
2022-12-22 20:10:25 +0000

    more example genes

--- misc/mut_frac/mut_frac.r
+++ misc/mut_frac/mut_frac.r
@@ -28,12 +28,13 @@ get_cnas = function(aneup) {
                       n_del = sum(gistic == -2),
                       `TP53 loss` = as.integer(gistic[gene == "TP53"] == -2),
     #                  `MYC amp` = as.integer(gistic[gene == "MYC"] == 2),
-                      `IFNA1 loss` = as.integer(gistic[gene == "IFNA1"] == -2)) %>%
-    #                  `CDKN2A loss` = as.integer(gistic[gene == "CDKN2A"] == -2)) %>%
+                      `IFNA1 loss` = as.integer(gistic[gene == "IFNA1"] == -2),
+                      `IFNB1 loss` = as.integer(gistic[gene == "IFNB1"] == -2),
+                      `CDKN2A loss` = as.integer(gistic[gene == "CDKN2A"] == -2)) %>%
         ungroup()
     xx = inner_join(aneup, cfrac) %>%
-        tidyr::gather("gene", "cna", -(Sample:n_del)) %>%
-        mutate(gene = factor(gene, levels=c("MYC amp", "IFNA1 loss", "TP53 loss")))
+        tidyr::gather("gene", "cna", -(Sample:n_del)) #%>%
+#        mutate(gene = factor(gene, levels=c("MYC amp", "IFNA1 loss", "TP53 loss")))
 }
 
 plot_cnas = function(xx) {
@@ -72,10 +73,12 @@ get_muts = function(aneup) {
     m = lapply(tcga$cohorts(), load_fl) %>%
         dplyr::bind_rows() %>%
         group_by(cohort, Sample) %>%
-            mutate(n = n()) %>%
+            mutate(n = n_distinct(gene)) %>%
             summarize(tot = unique(n),
-                      #STAT1 = as.integer("STAT1" %in% gene[vclass != "Silent"]),
+                      STAT1 = as.integer("STAT1" %in% gene[vclass != "Silent"]),
                       TP53 = as.integer("TP53" %in% gene[vclass != "Silent"]),
+                      JAK2 = as.integer("JAK2" %in% gene[vclass != "Silent"]),
+#                      B2M = as.integer("B2M" %in% gene[vclass != "Silent"]),
                       TTN = as.integer("TTN" %in% gene[vclass != "Silent"])) %>%
         ungroup()
     both = aneup %>%
@@ -106,7 +109,7 @@ plot_muts = function(both) {
         geom_boxplot(outlier.shape=NA) +
         facet_wrap(~ gene) +
         scale_color_manual(values=setNames(c("#cc9933", "#5500aa"), c("(0,0.1]", "(0.1,Inf]")), name="Aneuploidy") +
-        labs(x="Aneuploidy", y="As fraction of total mutational load") +
+        labs(x="Aneuploidy", y="As fraction of mutated genes") +
         scale_y_log10() +
         ggsignif::geom_signif(color="black", test=wilcox.test, comparisons=list(c("(0,0.1]", "(0.1,Inf]")))
 
