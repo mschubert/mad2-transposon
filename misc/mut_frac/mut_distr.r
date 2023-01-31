a4a97a10b7d7802b61b699d5b91d5b836f99b050
mschubertv <mschu.dev@gmail.com>
2023-01-31 12:03:50 +0000

    refactor plotting

--- misc/mut_frac/mut_distr.r
+++ misc/mut_frac/mut_distr.r
@@ -1,13 +1,12 @@
 library(dplyr)
 library(ggplot2)
 library(patchwork)
+plt = import('plot')
+gset = import('genesets')
 tcga = import('data/tcga')
 sys = import('sys')
 
-all_muts_volc = function(aneup, ex) {
-    plt = import('plot')
-    gset = import('genesets')
-
+get_muts = function(aneup, ex) {
     load_fl = function(coh) tcga$mutations(coh) %>%
         transmute(cohort=coh, Sample=Sample, gene=Hugo_Symbol, vclass=Variant_Classification)
     m = lapply(tcga$cohorts(), load_fl) %>%
@@ -18,31 +17,28 @@ all_muts_volc = function(aneup, ex) {
         ungroup() %>%
         filter(tot <= 5000) %>%
         inner_join(ex)
-    genes = table(m$gene)
+}
+
+test_fet = function(muts) {
+    genes = table(muts$gene)
     genes = names(genes)[genes > 50]
 
     test_one = function(g) {
-        df = m %>% filter(g == gene) %>% group_by(aneup_class, Sample, glen) %>% summarize(.gene = -log10(tot[1]))
+        df = muts %>% filter(g == gene) %>% group_by(aneup_class, Sample, glen) %>%
+            summarize(.gene = -log10(tot[1]))
         broom::tidy(lm(.gene ~ aneup_class, data=df)) %>% mutate(glen=df$glen[1])
     }
-    res = sapply(genes, test_one, simplify=FALSE) %>% bind_rows(.id="gene") %>%
+    sapply(genes, test_one, simplify=FALSE) %>% bind_rows(.id="gene") %>%
         filter(term == "aneup_class(0.1,Inf]") %>%
         mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
         arrange(adj.p, p.value) %>%
         filter(std.error < 0.2)
+}
+
+distr_plot = function(res) {
 #    res$circle = res$gene %in% c("JAK1", "STAT1", "B2M", "TP53", "TTN", "CDKN2A", "HLA-A", "HLA-B", "ERBB2", "KRAS", "MYC")
     res$circle = res$gene %in% c("JAK1", "STAT1", "TP53", "TTN")
-    plt$volcano(res, ceil=1e-15, label_top=50)
-
-    sets = gset$get_human(c("MSigDB_Hallmark_2020", "GO_Biological_Process_Tree"))
-    f1 = gset$test_fet(res$gene[res$estimate>quantile(res$estimate,0.7)], sets[[1]], valid=res$gene)
-    plt$volcano(f1 %>% mutate(estimate=log2(estimate)))
-    s1 = gset$test_lm(res, stat="estimate", sets[[1]])
-    plt$volcano(s1)
-    f2 = gset$test_fet(res$gene[res$estimate>quantile(res$estimate,0.7)], sets[[2]], valid=res$gene, min=4)
-    plt$volcano(f2 %>% mutate(estimate=log2(estimate)))
-    s2 = gset$test_lm(res, stat="est2", gset$filter(sets[[2]], max=250))
-    plt$volcano(s2)
+#    plt$volcano(res, ceil=1e-15, label_top=50)
 
     #TODO: figure out why TP53 vs. CDKN2A so different here
     res2 = res[res$circle,]
@@ -53,9 +49,34 @@ all_muts_volc = function(aneup, ex) {
 #        coord_cartesian(xlim=c(-2e-4, 7e-4))
 }
 
+volcano_hallmarks = function(res) {
+    sets_hm = gset$get_human("MSigDB_Hallmark_2020")
+    hits_hm = res$gene[res$estimate>quantile(res$estimate,0.7)]
+
+    fet_hm = gset$test_fet(hits_hm, sets_hm, valid=res$gene)
+    lm_hm = gset$test_lm(res, stat="estimate", sets_hm)
+
+    plt$volcano(fet_hm %>% mutate(estimate=log2(estimate))) + ggtitle("FET") |
+    plt$volcano(lm_hm) + ggtitle("lm")
+}
+
+volcano_go = function(res) {
+    sets_go = gset$get_human("GO_Biological_Process_Tree")
+    hits_go = res$gene[res$estimate>quantile(res$estimate,0.7)]
+
+    fet_go = gset$test_fet(hits_go, sets_go, valid=res$gene, min=4)
+    lm_go = gset$test_lm(res, stat="estimate", gset$filter(sets_go, max=250))
+
+    plt$volcano(fet_go %>% mutate(estimate=log2(estimate))) + ggtitle("FET") |
+    plt$volcano(lm_go) + ggtitle("lm")
+}
+
 sys$run({
     aneup = readRDS("mut_frac.rds")$aneup
     ex = readRDS("canonical_exon_length.rds")
 
-    all_muts_volc(aneup, ex)
+    muts = get_muts(aneup, ex)
+    res = test_fet(muts)
+
+    saveRDS(list(muts=muts, res=res), file="mut_distr.rds")
 })
