b46f51a8d078b723aa55b5f47310c2cbdd605d66
mschubertv <mschu.dev@gmail.com>
2024-01-18 13:39:27 +0000

    add sample sheet .tsv

--- data/rnaseq_stat1_2024/eset.r
+++ data/rnaseq_stat1_2024/eset.r
@@ -9,22 +9,21 @@ deseq = import('process/deseq')
 load_counts = function(count_file, samples) {
     seq_lib = tools::file_path_sans_ext(basename(count_file))
     message(seq_lib)
-    idx = samples[[seq_lib]]
-    counts = readRDS(count_file)[,names(idx),drop=FALSE]
-    colnames(counts) = sprintf("%s.%s", seq_lib, names(idx))
+    counts = readRDS(count_file)[,samples$sample,drop=FALSE]
+    colnames(counts) = sprintf("%s.%s", seq_lib, samples$short)
     counts
 }
 
 plot_pca = function(eset, ntop=500, title="PCA") {
     pr = deseq$prcomp(eset, ntop=ntop)
-    lines = as_tibble(as.data.frame(colData(eset))) %>%
+    lines = as_tibble(as.data.frame(colData(eset))) |>
         mutate(PC1 = pr$x[,"PC1"], PC2=pr$x[,"PC2"])
-    lines2 = lines %>%
-        group_by(sample) %>%
-        filter(n_distinct(batch) > 1)
+    lines2 = lines |>
+        group_by(sample) |>
+        filter(n_distinct(rep) > 1)
 
-    plt$pca(eset, aes(x=PC1, y=PC2, fill=short, color=batch), pr=pr) +
-        geom_line(data=lines, aes(group=short), color="black", alpha=0.1) +
+    plt$pca(eset, aes(x=PC1, y=PC2, fill=paste(cline, genotype), color=rep), pr=pr) +
+        geom_line(data=lines, aes(group=treatment), color="black", alpha=0.1) +
         geom_point(shape=21, size=3, color="#12121256") +
         scale_fill_brewer(palette="Paired") +
         scale_color_brewer(palette="Dark2") +
@@ -44,23 +43,19 @@ plot_hallmark_gsva = function(vs) {
 
 sys$run({
     args = sys$cmd$parse(
-        opt('s', 'samples', 'yaml', 'fastq_id.yaml'),
+        opt('s', 'samples', 'tsv', 'samples.tsv'),
         opt('o', 'outfile', 'rds', 'eset.rds'),
         opt('p', 'plotfile', 'pdf', 'eset.pdf'),
         arg('infiles', 'rds', arity='*',
             list.files('seq_counts', '\\.rds$', recursive=TRUE, full.names=TRUE))
     )
 
-    samples = yaml::read_yaml("fastq_id.yaml") %>% lapply(unlist)
-    counts = lapply(args$infiles, load_counts, samples=samples) %>%
+    meta = readr::read_tsv(args$samples) |>
+        mutate(short = sprintf("%s %s %s %sh-%i", cline, genotype, conc, hours, rep),
+               rep = factor(rep))
+    counts = lapply(args$infiles, load_counts, samples=meta) |>
         narray::stack(along=2, fill=0)
 
-    meta = tibble(sample = unlist(samples, use.names=FALSE),
-                  batch = sub("^(.+)\\.[^.]+", "\\1", colnames(counts))) %>%
-        mutate(#genotype = sub("([^-]+) hours-(.+)", "\\1", sample),
-               #treatment = sub("([^-]+) hours-(.+)", "\\2", sample),
-               #rep = sub("([^-]+) hours-(.+)", "\\2", sample),
-               short = sub(" nM", "", sub(" hours", "", sample)))
     eset = DESeq2::DESeqDataSetFromMatrix(counts, meta, ~1)
     vs = DESeq2::varianceStabilizingTransformation(eset)
 
